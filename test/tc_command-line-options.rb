e911e5c2e13e18d236f4828f89c7cd354cd92ae8
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-01-31 07:35:03 +0000

    Id と LastChangedRevision の置換を指定

--- test/tc_command-line-options.rb
+++ test/tc_command-line-options.rb
@@ -1,8 +1,8 @@
 #
-# $Id: tc_command-line-options.rb 14 2005-01-19 06:32:24Z takamiya $
+# $Id$
 #
 # Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
-# Revision:: $Revision: 1.6 $
+# Revision:: $LastChangedRevision$
 # License::  GPL2
 
 $LOAD_PATH.unshift './lib'

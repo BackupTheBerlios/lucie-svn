36d5c1071200e6d8f0385d0a9da5dd8a9126207c
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-01 06:43:03 +0000

    * 不要なテストランナー用コードを削除 * ロードパスの修正 * SVN キーワードの修正

--- test/tc_time-stamp.rb
+++ test/tc_time-stamp.rb
@@ -1,11 +1,11 @@
 #
-# $Id: tc_time-stamp.rb 14 2005-01-19 06:32:24Z takamiya $
+# $Id$
 #
 # Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
-# Revision:: $Revision: 1.6 $
+# Revision:: $LastChangedRevision$
 # License::  GPL2
 
-$LOAD_PATH.unshift '../lib'
+$LOAD_PATH.unshift './lib'
 
 require 'lucie/time-stamp'
 require 'test/unit'
@@ -15,16 +15,6 @@ class TC_TimeStamp < Test::Unit::TestCase
   def test_svn_date
     assert_match( /\A\d\d\d\d-\d\d-\d\d\Z/, Lucie::svn_date, "Lucie::svn_date returned wrong date format String." )
   end
-end
-
-# FIXME
-# o 以下のブロックをテストで共通化する。
-# o TestRunner の種類を Rake のターゲットなどで選択可能なようにする。
-if __FILE__ == $0
-  require 'test/unit/ui/tk/testrunner'
-  require 'test/unit/testsuite'
-
-  Test::Unit::UI::Tk::TestRunner.run( TC_TimeStamp )
 end
 
 ### Local variables:

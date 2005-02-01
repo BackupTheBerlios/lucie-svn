d1b4d024e225ff4f3e8531e789989dcbbe651165
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-01 05:23:39 +0000

    定数のテストを追加

--- test/tc_setup.rb
+++ test/tc_setup.rb
@@ -11,6 +11,21 @@ require 'lucie/lucie-setup'
 require 'test/unit'
 
 class TC_Setup < Test::Unit::TestCase
+  # LUCIE_VERSION 定数をテスト
+  public
+  def test_LUCIE_VERSION
+    assert_match( /\A\d+\.\d+\.\d+\Z/, Lucie::Setup::LUCIE_VERSION,
+     "LUCIE_VERSION 定数のフォーマットがおかしい" )
+  end
+  
+  # VERSION_STRING 定数をテスト
+  public
+  def test_VERSION_STRING
+    assert_match( /\Alucie-setup\s+\d+\.\d+\.\d+\s+\(\d\d\d\d-\d\d-\d\d\)\Z/, Lucie::Setup::VERSION_STRING, 
+    "VERSION_STRING 定数のフォーマットがおかしい" )
+  end
+  
+  # シングルトンであることをテスト
   public
   def test_singleton
     assert Lucie::Setup::include?( Singleton )

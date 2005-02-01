db4da9d48e24bbddf57cd83aea207c5a4225d8d9
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-01 05:08:35 +0000

    * 不要なテストランナー用コードを削除 * ロードパスの修正 * SVN キーワードの修正

--- test/tc_setup.rb
+++ test/tc_setup.rb
@@ -1,11 +1,11 @@
 #
-# $Id: tc_setup.rb 14 2005-01-19 06:32:24Z takamiya $
+# $Id$
 #
 # Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
-# Revision:: $Revision: 1.6 $
+# Revision:: $LastChangedRevision$
 # License::  GPL2
 
-$LOAD_PATH.unshift '../lib'
+$LOAD_PATH.unshift './lib'
 
 require 'lucie/lucie-setup'
 require 'test/unit'
@@ -15,16 +15,6 @@ class TC_Setup < Test::Unit::TestCase
   def test_singleton
     assert Lucie::Setup::include?( Singleton )
   end
-end
-
-# FIXME
-# o 以下のブロックをテストで共通化する。
-# o TestRunner の種類を Rake のターゲットなどで選択可能なようにする。
-if __FILE__ == $0
-  require 'test/unit/ui/tk/testrunner'
-  require 'test/unit/testsuite'
-
-  Test::Unit::UI::Tk::TestRunner.run( TC_Setup )
 end
 
 ### Local variables:

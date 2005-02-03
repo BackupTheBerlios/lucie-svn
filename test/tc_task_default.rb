54ecad247af3f4b387b438b0d36e376296bdabfa
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-03 09:30:07 +0000

    リファクタリング

--- test/tc_task_default.rb
+++ test/tc_task_default.rb
@@ -12,16 +12,14 @@ require 'test/unit'
 
 class TC_Task_Default < Test::Unit::TestCase
   public
-  def test_default_task_defined
-    assert_equal ["installer_template"], default_task.prerequisites, "Prerequisites が正しくない"
+  def test_default_task_defined_and_prerequisites
+    assert_equal ['installer_template'], default_task.prerequisites, 'default タスクの Prerequisites が正しくない'
   end
   
   private
   def default_task
-    ObjectSpace.each_object( Task ) do |each|
-      return each if each.name == "default"
-    end
-    fail "default タスクが定義されていない"
+    fail 'default タスクが定義されていない' unless Task::task_defined?( 'default' )
+    return Task::lookup( 'default' )
   end
 end
 

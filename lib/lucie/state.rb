bb6cdcfffb02226aac2e81280b74015325ea0082
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-07 04:30:20 +0000

    State -> Lucie::State に変更

--- lib/lucie/state.rb
+++ lib/lucie/state.rb
@@ -7,55 +7,58 @@
 
 require 'lucie/time-stamp'
 
-# 状態遷移表から State パターンの各 ConcreteState クラスを生成する。
-#--
-# TODO: 各 ConcreteState は特異オブジェクトにする。
-#++
-class State
-  Lucie::update(%q$Date$)
+module Lucie
   
-  PRIORITY_LOW = 'low'.freeze
-  PRIORITY_MEDIUM = 'medium'.freeze
-  PRIORITY_HIGH = 'high'.freeze
-  PRIORITY_CRITICAL = 'critical'.freeze
+  update(%q$Date$)
   
-  # あたらしい State をかえす。
-  public
-  def initialize( questionString, priorityString )
-    raise TypeError, "First argument must be a String." unless questionString.kind_of?( String )
-    raise Exception::UnsupportedPriorityException, "Unsupported priority : #{priorityString}" unless valid_priority?( priorityString ) 
-    
-    @question = questionString
-    @priority = priorityString
-  end
-  
-  # 次の State に遷移する
-  public
-  def transit( aDebconfContext )
-    raise NotImplementedError, 'abstract method'
-  end
-  
-  # Ruby のスクリプトを返す
-  public
-  def self.marshal( aQuestion )
-    raise NotImplementedError, 'abstract method'
-  end
-  
-  private
-  def supported_priorities
-    [ PRIORITY_LOW, PRIORITY_MEDIUM, PRIORITY_HIGH, PRIORITY_CRITICAL ]
-  end
-  
-  private
-  def valid_priority?( priorityString )
-    supported_priorities.include? priorityString
-  end
-  
+  # 状態遷移表から State パターンの各 ConcreteState クラスを生成する。
   #--
-  # TODO: need to implement Exception & Error using exception
-  #++
-  module Exception #:nodoc:
-    class UnsupportedPriorityException < ::Exception #:nodoc:
+  # TODO: 各 ConcreteState は特異オブジェクトにする。
+  #++  
+  class State    
+    PRIORITY_LOW = 'low'.freeze
+    PRIORITY_MEDIUM = 'medium'.freeze
+    PRIORITY_HIGH = 'high'.freeze
+    PRIORITY_CRITICAL = 'critical'.freeze
+    
+    # あたらしい State をかえす。
+    public
+    def initialize( questionString, priorityString )
+      raise TypeError, "First argument must be a String." unless questionString.kind_of?( String )
+      raise Exception::UnsupportedPriorityException, "Unsupported priority : #{priorityString}" unless valid_priority?( priorityString ) 
+      
+      @question = questionString
+      @priority = priorityString
+    end
+    
+    # 次の State に遷移する
+    public
+    def transit( aDebconfContext )
+      raise NotImplementedError, 'abstract method'
+    end
+    
+    # Ruby のスクリプトを返す
+    public
+    def self.marshal( aQuestion )
+      raise NotImplementedError, 'abstract method'
+    end
+    
+    private
+    def supported_priorities
+      [ PRIORITY_LOW, PRIORITY_MEDIUM, PRIORITY_HIGH, PRIORITY_CRITICAL ]
+    end
+    
+    private
+    def valid_priority?( priorityString )
+      supported_priorities.include? priorityString
+    end
+    
+    #--
+    # TODO: need to implement Exception & Error using exception
+    #++
+    module Exception #:nodoc:
+      class UnsupportedPriorityException < ::Exception #:nodoc:
+      end
     end
   end
 end
--- test/tc_state.rb
+++ test/tc_state.rb
@@ -14,32 +14,32 @@ class TC_State < Test::Unit::TestCase
   # 定数のテスト
   public 
   def test_PRIORITY_LOW
-    assert_match /\A\S+\Z/, State::PRIORITY_LOW, "PRIORITY_LOW 定数が正しく定義されていなかった"
+    assert_match /\A\S+\Z/, Lucie::State::PRIORITY_LOW, "PRIORITY_LOW 定数が正しく定義されていなかった"
   end
   
   # 定数のテスト  
   public 
   def test_PRIORITY_MEDIUM
-    assert_match /\A\S+\Z/, State::PRIORITY_MEDIUM, "PRIORITY_MEDIUM 定数が正しく定義されていなかった"
+    assert_match /\A\S+\Z/, Lucie::State::PRIORITY_MEDIUM, "PRIORITY_MEDIUM 定数が正しく定義されていなかった"
   end
   
   # 定数のテスト  
   public 
   def test_PRIORITY_HIGH
-    assert_match /\A\S+\Z/, State::PRIORITY_HIGH, "PRIORITY_HIGH 定数が正しく定義されていなかった"
+    assert_match /\A\S+\Z/, Lucie::State::PRIORITY_HIGH, "PRIORITY_HIGH 定数が正しく定義されていなかった"
   end
   
   # 定数のテスト  
   public 
   def test_PRIORITY_CRITICAL
-    assert_match /\A\S+\Z/, State::PRIORITY_CRITICAL, "PRIORITY_CRITICAL 定数が正しく定義されていなかった"
+    assert_match /\A\S+\Z/, Lucie::State::PRIORITY_CRITICAL, "PRIORITY_CRITICAL 定数が正しく定義されていなかった"
   end
   
   # transit メソッドがアブストラクトであることを確認
   public
   def test_transit_not_implemented
     assert_raises( NotImplementedError, "NotImplementedError が raise されなかった" ) do
-      State.new( "DUMMY STATE", State::PRIORITY_LOW ).transit nil
+      Lucie::State.new( "DUMMY STATE", Lucie::State::PRIORITY_LOW ).transit nil
     end
   end
   
@@ -47,15 +47,16 @@ class TC_State < Test::Unit::TestCase
   public
   def test_marshal_not_implemented
     assert_raises( NotImplementedError, "NotImplementedError が raise されなかった" ) do
-      State::marshal nil
+      Lucie::State::marshal nil
     end
   end
   
   # priority として正しくないものを渡したときの Exception を確認
   public
   def test_exception_with_unsupported_priority
-    assert_raises( State::Exception::UnsupportedPriorityException, "State::Exception::UnsupportedPriorityException が raise されなかった" ) do
-      State.new( "QUESTION", "UNSUPPORTED PRIORITY" )
+    assert_raises( Lucie::State::Exception::UnsupportedPriorityException, 
+                   "Lucie::State::Exception::UnsupportedPriorityException が raise されなかった" ) do
+      Lucie::State.new( "QUESTION", "UNSUPPORTED PRIORITY" )
     end
   end
   
@@ -63,7 +64,7 @@ class TC_State < Test::Unit::TestCase
   public
   def test_exception_with_invalid_type_of_question
     assert_raises( TypeError, "TypeError が raise されなかった" ) do
-      State.new( nil, State::PRIORITY_LOW )
+      Lucie::State.new( nil, Lucie::State::PRIORITY_LOW )
     end
   end
 end

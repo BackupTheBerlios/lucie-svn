fe73a31eda3f7a664d808099e9373d5b31cb7160
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-09 08:58:37 +0000

    追加

---
+++ lib/lucie.rb
@@ -0,0 +1,17 @@
+# = Lucie ライブラリのメインファイル
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision$
+# License::  GPL2
+
+# LMP 設定ファイルの頭では本ファイル (lucie.rb) をかならず require し、
+# template, question などのトップレベル関数を読み込むこと。
+
+require 'lucie/template'
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

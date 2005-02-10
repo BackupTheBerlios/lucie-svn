57a02cedbcd684ca3cd8ff3c44a812a5818929bf
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-10 14:36:50 +0000

    追加

---
+++ lib/lucie/string-template.rb
@@ -0,0 +1,24 @@
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision$
+# License::  GPL2
+
+$KCODE = 'SJIS'
+
+require 'lucie/template'
+require 'lucie/time-stamp'
+
+module Lucie
+  
+  update(%q$LastChangedDate$)
+  
+  class StringTemplate < Template
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:
---
+++ test/tc_string_template.rb
@@ -0,0 +1,21 @@
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision$
+# License::  GPL2
+
+$KCODE = 'SJIS'
+$LOAD_PATH.unshift './lib'
+
+require 'lucie/string-template'
+require 'test/unit'
+
+class TC_StringTemplate < Test::Unit::TestCase
+end
+
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

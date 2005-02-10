aa2a0b2f9a61f62efaa7ce6aa359eda8d8bce6a3
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-10 09:52:51 +0000

    to_s メソッドを Template -> BooleanTemplate へ移動

--- lib/lucie/boolean-template.rb
+++ lib/lucie/boolean-template.rb
@@ -5,6 +5,8 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
+$KCODE = 'SJIS'
+
 require 'lucie/template'
 require 'lucie/time-stamp'
 
@@ -13,7 +15,71 @@ module Lucie
   update(%q$LastChangedDate$)
   
   class BooleanTemplate < Template
+    # BooleanTemplate をあらわす String オブジェクトを返します
+    public
+    def to_s
+      template_string =  "Template: #{name}\n"
+      template_string += "Type: boolean\n"
+      if description
+        template_string += "Description: #{short_description}\n"
+        template_string += long_description + "\n"
+      end
+        
+      if description_ja
+        template_string += "Description-ja: #{short_description_ja}\n" 
+        template_string += long_description_ja
+      end
+    end
+    
+    #--
+    # FIXME : Lucie::Template クラスへ移動
+    #++
+    private
+    def long_description
+      return format_long_description( description )
+    end
+    
+    #--
+    # FIXME : Lucie::Template クラスへ移動
+    #++
+    private 
+    def long_description_ja
+      return format_long_description( description_ja )
+    end
+    
+    #--
+    # FIXME : Lucie::Template クラスへ移動
+    #++
+    private
+    def short_description_ja
+      description_ja.split("\n")[0]
+    end
+    
+    #--
+    # FIXME : Lucie::Template クラスへ移動
+    #++
+    private
+    def short_description
+      description.split("\n")[0]
+    end
+    
+    #--
+    # FIXME : Lucie::Template クラスへ移動
+    #++
+    private
+    def format_long_description( descriptionString )
+      return descriptionString.split("\n")[1..-1].map do |each|
+        case each
+        when /\A\s*\Z/
+          ' .'
+        else
+          " #{each}"
+        end
+      end.join("\n")
+    end
   end
+  
+  BOOLEAN = Lucie::BooleanTemplate
 end
 
 ### Local variables:
--- test/tc_boolean_template.rb
+++ test/tc_boolean_template.rb
@@ -5,6 +5,7 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
+$KCODE = 'SJIS'
 $LOAD_PATH.unshift './lib'
 
 require 'lucie/boolean-template'
@@ -15,6 +16,32 @@ class TC_BooleanTemplate < Test::Unit::TestCase
   public
   def test_inheritance
     assert Lucie::BooleanTemplate < Lucie::Template
+  end
+  
+  public
+  def test_to_s
+    Lucie::Template.clear
+    template( 'TEST/BOOLEAN-TEMPLATE', Lucie::BOOLEAN ) do |template|
+      template.default = 'no'
+      template.description = (<<-DESCRIPTION)
+      This is a short description
+      This is a long description
+      
+      the abobe is a null line
+      DESCRIPTION
+      template.description_ja = (<<-DESCRIPTION_JA)
+      これは短いデスクリプションです
+      これは長いデスクリプションです
+      
+      上は空行です
+      DESCRIPTION_JA
+    end.register
+    
+    assert /^Template:(.*)^Type:(.*)^Description:(.*)^Description-ja:(.*)/m=~ Lucie::Template['TEST/BOOLEAN-TEMPLATE'].to_s
+    assert_match /TEST\/BOOLEAN-TEMPLATE/, $1, 'Template: の値がおかしい'
+    assert_match /boolean/, $2, 'Type: の値がおかしい'
+    assert_match /This is a short description.*This is a long description.*the abobe is a null line/m, $3, 'Description: の値がおかしい'
+    assert_match /これは短いデスクリプションです.*これは長いデスクリプションです.*上は空行です/m, $4, 'Description-ja: の値がおかしい'
   end
 end
 

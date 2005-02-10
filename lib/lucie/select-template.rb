c6e39d7ba7990ea6fcd398962d15e50b484516f6
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-10 15:44:15 +0000

    Type: select 関係の機能を SelectTemplate クラスに分離

---
+++ lib/lucie/select-template.rb
@@ -0,0 +1,40 @@
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision$
+# License::  GPL2
+
+$KCODE = 'SJIS'
+
+require 'lucie/template'
+require 'lucie/time-stamp'
+
+module Lucie
+  
+  update(%q$LastChangedDate$)
+  
+  class SelectTemplate < Template
+    # SelectTemplate をあらわす String オブジェクトを返します
+    public
+    def to_s
+      template_string =  "Template: #{name}\n"
+      template_string += "Type: select\n"
+      template_string += "Choices: #{choices}\n"
+      template_string += "Default: #{default}\n" if default
+      if description
+        template_string += "Description: #{short_description}\n"
+        template_string += long_description + "\n"
+      end      
+      if description_ja
+        template_string += "Description-ja: #{short_description_ja}\n" 
+        template_string += long_description_ja
+      end
+    end
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

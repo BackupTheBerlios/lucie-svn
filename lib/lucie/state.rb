9d8afda7cdca8f13f8cace9e2e67fcd95889debe
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-14 03:54:33 +0000

    define_state メソッドを追加

--- lib/lucie/state.rb
+++ lib/lucie/state.rb
@@ -27,6 +27,41 @@ module Lucie
     def self.marshal( aQuestion )
       raise NotImplementedError, 'abstract method'
     end
+    
+    #--
+    # FIXME : ポリモルフィズム
+    #++
+    public
+    def self.define_state( aQuestion )
+      case aQuestion.template
+      when StringTemplate
+        require 'lucie/string-state'
+        eval StringState.marshal( aQuestion )
+        return eval( "#{aQuestion.name.to_state_class_name}.new" )        
+      when MultiselectTemplate
+        require 'lucie/multiselect-state'
+        eval MultiselectState.marshal( aQuestion )
+        return eval( "#{aQuestion.name.to_state_class_name}.new( aQuestion )" )      
+      when SelectTemplate
+        require 'lucie/select-state'
+        eval SelectState.marshal( aQuestion )
+        return eval( "#{aQuestion.name.to_state_class_name}.new( aQuestion )" )     
+      when NoteTemplate
+        require 'lucie/note-state'
+        eval NoteState.marshal( aQuestion )
+        return eval( "#{aQuestion.name.to_state_class_name}.new" )
+      when BooleanTemplate
+        require 'lucie/boolean-state'
+        eval BooleanState.marshal( aQuestion )
+        return eval( "#{aQuestion.name.to_state_class_name}.new( aQuestion )" )      
+      when TextTemplate
+        require 'lucie/text-state'
+        eval TextState.marshal( aQuestion )
+        return eval( "#{aQuestion.name.to_state_class_name}.new" )
+      else
+        raise "This shouldn't happen!"
+      end
+    end
   end
 end
 

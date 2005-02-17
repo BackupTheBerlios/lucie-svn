be154d83c50ec3c5964bdbcea6dee9f311315a0a
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-17 13:40:46 +0000

    o transit のテストを追加 o リファクタリング

--- lib/deft/debconf-context.rb
+++ lib/deft/debconf-context.rb
@@ -5,39 +5,55 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-require 'debconf/client'
-require 'deft/state'
 require 'English'
 require 'lucie'
-
-include Debconf::ConfModule
 
 module Deft
   
   # FIXME : update メソッドを Deft モジュールへ移動
   Lucie.update(%q$Date$)
   
+  # Debconf による画面遷移を表すクラス
   class DebconfContext
+    # Debconf の各画面を表す Concrete State のハッシュ
     STATES = {}
-    attr_accessor :current_state
     
+    # 現在の Question オブジェクト
+    attr_accessor :current_question
+    # 現在の Concrete State
+    attr_accessor :current_state
+    # Debconf との通信に用いる標準入力 (デバッグ用。Test Case 中 で Mock などを代入する)
+    attr_accessor :stdin
+    # Debconf との通信に用いる標準出力 (デバッグ用。Test Case 中 で Mock などを代入する)
+    attr_accessor :stdout
+    
+    # あたらしい DebconfContext オブジェクトを返す
     public
     def initialize
-      Lucie::Question::QUESTIONS.values.each do |each|
-        STATES[each.name] = State.concrete_state( each )
-      end
-      @current_state = get_start_state
+      register_concrete_state
+      @current_question = get_start_question
+      @current_state = STATES[@current_question.name]
+      @stdout = STDOUT
+      @stdin = STDIN
     end
     
+    # 次の質問へ遷移する
     public
     def transit
       @current_state.transit self
     end
     
     private
-    def get_start_state
+    def register_concrete_state
       Lucie::Question::QUESTIONS.values.each do |each|
-        return STATES[each.name] if each.first_question
+        STATES[each.name] = each.concrete_state
+      end
+    end
+    
+    private
+    def get_start_question
+      Lucie::Question::QUESTIONS.values.each do |each|        
+        return each if each.first_question
       end
     end
   end
--- test/deft/tc_debconf-context.rb
+++ test/deft/tc_debconf-context.rb
@@ -6,24 +6,46 @@
 # License::  GPL2
 
 ENV["DEBIAN_HAS_FRONTEND"] = 'true' # debconf/client.rb のエラーを回避
-
 $LOAD_PATH.unshift './lib'
 
+require 'mock'
 require 'deft/debconf-context'
 require 'test/unit'
 
-class TC_DebconfContext < Test::Unit::TestCase
+class TC_DebconfContext < Test::Unit::TestCase  
   #--
-  # FIXME : Question と対応する Template の require の順でエラーが起こる。Template の無い Question を定義しようとすると例外を投げるようにする。
+  # FIXME : Question と対応する Template の require の順でエラーが起こるのを修正
   #++
   public
-  def test_start_state
+  def setup
     require 'test/lmp_conf/lucie_vm_template'
-    require 'test/lmp_conf/lucie_vm_question'    
+    require 'test/lmp_conf/lucie_vm_question'      
+    @debconf_context = Deft::DebconfContext.new
+  end
+  
+  # transit で次の State へ遷移することを確認
+  public
+  def test_transit
+    stdout = Mock.new( '[STDOUT]' )
+    stdout.__next( :print ) do |output| assert_equal "INPUT medium lucie-vmsetup/hello\n", output end
+    stdout.__next( :print ) do |output| assert_equal "GO\n", output end
     
-    debconf_context = Deft::DebconfContext.new
-    assert_kind_of Deft::State, debconf_context.current_state
-    assert_equal 'Deft::State::LucieVmsetup__Hello', debconf_context.current_state.class.to_s
+    stdin = Mock.new( '[STDIN]' )
+    stdin.__next( :gets ) do || '0 TRUE' end
+    stdin.__next( :gets ) do || '0 TRUE' end
+    
+    @debconf_context.stdout = stdout    
+    @debconf_context.stdin = stdin    
+    @debconf_context.transit
+    assert_equal 'lucie-vmsetup/num-nodes', @debconf_context.current_question.name
+    assert_equal 'Deft::State::LucieVmsetup__NumNodes', @debconf_context.current_state.class.to_s
+  end
+  
+  # 状態遷移の開始地点の State が取得できることを確認
+  public
+  def test_start_state
+    assert_kind_of Deft::State, @debconf_context.current_state
+    assert_equal 'Deft::State::LucieVmsetup__Hello', @debconf_context.current_state.class.to_s
   end
 end
 

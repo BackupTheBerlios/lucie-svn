9b0a73f53e9fa0427322b249867522fb012fc5bc
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-17 08:16:57 +0000

    Deft モジュールへ移動

--- lib/deft/debconf-context.rb
+++ lib/deft/debconf-context.rb
@@ -6,35 +6,35 @@
 # License::  GPL2
 
 require 'lucie'
-require 'lucie/state'
+require 'deft/state'
 require 'debconf/client'
 require 'English'
-require 'lucie_vm_template'
-require 'lucie_vm_question'
 
 include Debconf::ConfModule
 
-class DebconfContext
-  STATES = {}
-  attr_accessor :current_state
-  
-  public
-  def initialize
-    Lucie::Question::QUESTIONS.values.each do |each|
-      STATES[each.name] = State.define_state( each )
+module Deft
+  class DebconfContext
+    STATES = {}
+    attr_accessor :current_state
+    
+    public
+    def initialize
+      Lucie::Question::QUESTIONS.values.each do |each|
+        STATES[each.name] = State.concrete_state( each )
+      end
+      @current_state = get_start_state
     end
-    @current_state = get_start_state
-  end
-  
-  public
-  def transit
-    @current_state.transit self
-  end
-  
-  private
-  def get_start_state
-    Lucie::Question::QUESTIONS.values.each do |each|
-      return STATES[each.name] if each.first_question
+    
+    public
+    def transit
+      @current_state.transit self
+    end
+    
+    private
+    def get_start_state
+      Lucie::Question::QUESTIONS.values.each do |each|
+        return STATES[each.name] if each.first_question
+      end
     end
   end
 end
--- test/deft/tc_debconf-context.rb
+++ test/deft/tc_debconf-context.rb
@@ -6,11 +6,10 @@
 # License::  GPL2
 
 ENV["DEBIAN_HAS_FRONTEND"] = 'true' # debconf/client.rb ‚ÌƒGƒ‰[‚ğ‰ñ”ğ
-ENV["DEBCONF_RUBY_STDOUT"] = '1'
 
 $LOAD_PATH.unshift './lib'
 
-require 'lucie/debconf-context'
+require 'deft/debconf-context'
 require 'test/unit'
 
 class TC_DebconfContext < Test::Unit::TestCase
@@ -19,9 +18,9 @@ class TC_DebconfContext < Test::Unit::TestCase
     require 'test/lmp_conf/lucie_vm_template'
     require 'test/lmp_conf/lucie_vm_question'
     
-    debconf_context = DebconfContext.new
-    assert_kind_of Lucie::State, debconf_context.current_state
-    assert_equal 'Lucie::State::LucieVmsetup__Hello', debconf_context.current_state.class.to_s
+    debconf_context = Deft::DebconfContext.new
+    assert_kind_of Deft::State, debconf_context.current_state
+    assert_equal 'Deft::State::LucieVmsetup__Hello', debconf_context.current_state.class.to_s
   end
 end
 

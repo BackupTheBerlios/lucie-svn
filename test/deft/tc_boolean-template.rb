04ff610c42a42efc436de99f9d9a8a55f64142e2
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-18 07:03:54 +0000

    Lucie -> Deft にモジュールが移動したのに追従

--- test/deft/tc_boolean-template.rb
+++ test/deft/tc_boolean-template.rb
@@ -8,15 +8,15 @@
 $KCODE = 'SJIS'
 $LOAD_PATH.unshift './lib'
 
-require 'lucie/boolean-template'
+require 'deft/boolean-template'
 require 'test/unit'
 
 class TC_BooleanTemplate < Test::Unit::TestCase
   public
   def test_to_s
-    Lucie::Template.clear
+    Deft::Template.clear
     template( 'TEST/BOOLEAN-TEMPLATE' ) do |template|
-      template.template_type = Lucie::BooleanTemplate
+      template.template_type = Deft::BooleanTemplate
       template.default = 'true'
       template.short_description = 'This is a short description'
       template.extended_description = (<<-DESCRIPTION)      
@@ -31,7 +31,7 @@ class TC_BooleanTemplate < Test::Unit::TestCase
       上は空行です
       DESCRIPTION_JA
     end
-    assert /^Template:(.*)^Type:(.*)^Default:(.*)^Description:(.*)^Description-ja:(.*)/m=~ Lucie::Template['TEST/BOOLEAN-TEMPLATE'].to_s
+    assert /^Template:(.*)^Type:(.*)^Default:(.*)^Description:(.*)^Description-ja:(.*)/m=~ Deft::Template['TEST/BOOLEAN-TEMPLATE'].to_s
     assert_match /TEST\/BOOLEAN-TEMPLATE/, $1, 'Template: の値がおかしい'
     assert_match /boolean/, $2, 'Type: の値がおかしい'
     assert_match /true/, $3, 'Default: の値がおかしい'

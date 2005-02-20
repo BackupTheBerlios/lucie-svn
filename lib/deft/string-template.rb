d777bdc7b88de3c484ee9fb6781ff6b3a1b209dc
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-20 13:36:36 +0000

    AbstractTemplate に対応

--- lib/deft/string-template.rb
+++ lib/deft/string-template.rb
@@ -5,41 +5,19 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-$KCODE = 'SJIS'
-
+require 'deft/abstract-template'
 require 'deft/template'
-require 'lucie/time-stamp'
+require 'time-stamp'
+
+update(%q$LastChangedDate$)
 
 module Deft
-  
-  Lucie.update(%q$LastChangedDate$)
-  
   # string タイプの Template をあらわすクラス
-  class StringTemplate < Template
-    def_delegator :@template, :name
-    def_delegator :@template, :default
-    def_delegator :@template, :short_description
-    def_delegator :@template, :extended_description
-    def_delegator :@template, :short_description_ja
-    def_delegator :@template, :extended_description_ja    
-
-    # 新しい StringTemplate オブジェクトを返します
-    public
-    def initialize( aTemplate )
-      @template = aTemplate
-    end
-        
-    # StringTemplate をあらわす String オブジェクトを返します
+  class StringTemplate < AbstractTemplate   
+    # StringTemplate をあらわす String オブジェクトを返す
     public
     def to_s
-      template_string =  "Template: #{name}\n"
-      template_string += "Type: string\n"
-      template_string += "Default: #{default}\n" if default
-      template_string += "Description: #{short_description}\n" if short_description
-      template_string += format_extended_description( extended_description ) + "\n" if extended_description
-      template_string += "Description-ja: #{short_description_ja}\n" if short_description_ja
-      template_string += format_extended_description( extended_description_ja ) if extended_description_ja
-      return template_string
+      return template_string( 'string' )
     end
   end
 end
--- test/deft/tc_string-template.rb
+++ test/deft/tc_string-template.rb
@@ -5,25 +5,17 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-$KCODE = 'SJIS'
 $LOAD_PATH.unshift './lib'
 
 require 'deft/string-template'
 require 'test/unit'
 
 class TC_StringTemplate < Test::Unit::TestCase
-  # 親クラスが Deft::Template であることをテスト
-  public
-  def test_inheritance
-    assert Deft::StringTemplate < Deft::Template
-  end
-  
   public
   def test_to_s
     Deft::Template.clear
     template( 'TEST/STRING-TEMPLATE' ) do |template|
       template.template_type = Deft::StringTemplate
-      template.default = 'default'
       template.short_description = 'This is a short description'
       template.extended_description = (<<-DESCRIPTION)
       This is a long description
@@ -37,12 +29,11 @@ class TC_StringTemplate < Test::Unit::TestCase
       上は空行です
       DESCRIPTION_JA
     end
-    assert /^Template:(.*)^Type:(.*)^Default:(.*)^Description:(.*)^Description-ja:(.*)/m=~ Deft::Template['TEST/STRING-TEMPLATE'].to_s
+    assert /^Template:(.*)^Type:(.*)^Description:(.*)^Description-ja:(.*)/m=~ Deft::Template['TEST/STRING-TEMPLATE'].to_s
     assert_match /TEST\/STRING-TEMPLATE/, $1, 'Template: の値がおかしい'
     assert_match /string/, $2, 'Type: の値がおかしい'
-    assert_match /default/, $3, 'Default: の値がおかしい'
-    assert_match /This is a short description.*This is a long description.*the abobe is a null line/m, $4, 'Description: の値がおかしい'
-    assert_match /これは短いデスクリプションです.*これは長いデスクリプションです.*上は空行です/m, $5, 'Description-ja: の値がおかしい'
+    assert_match /This is a short description.*This is a long description.*the abobe is a null line/m, $3, 'Description: の値がおかしい'
+    assert_match /これは短いデスクリプションです.*これは長いデスクリプションです.*上は空行です/m, $4, 'Description-ja: の値がおかしい'
   end
 end
 

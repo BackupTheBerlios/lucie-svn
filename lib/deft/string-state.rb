0622093e6249cd7a259602992c6a7a83cae76fd9
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-20 14:26:31 +0000

    リファクタリング

--- lib/deft/string-state.rb
+++ lib/deft/string-state.rb
@@ -5,24 +5,24 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-require 'lucie/state'
+require 'deft/state'
 require 'lucie/string'
-require 'lucie/time-stamp'
+require 'time-stamp'
+
+update(%q$Date$)
 
 module Deft
-  
-  Lucie.update(%q$Date$)
-  
   # string 型テンプレート変数の質問が表示されている状態を表すクラス
   class StringState < State
     # Question オブジェクトから対応する StringState クラスの子クラスをあらわす文字列を返す
-    def self.marshal( aQuestion )
+    def self.marshal_concrete_state( aQuestion )
       return ( <<-CLASS_DEFINITION ).unindent_auto
-      class #{aQuestion.name.to_state_class_name} < Deft::StringState
+      class #{aQuestion.state_class_name} < Deft::StringState
         public
         def transit( aDebconfContext )
           super aDebconfContext
-          aDebconfContext.current_state = DebconfContext::STATES['#{aQuestion.next_question}']
+          aDebconfContext.current_question = Deft::Question['#{aQuestion.next_question}']
+          aDebconfContext.current_state    = Deft::ConcreteState['#{aQuestion.next_question}']
         end
       end
       CLASS_DEFINITION
--- test/deft/tc_string-state.rb
+++ test/deft/tc_string-state.rb
@@ -24,16 +24,18 @@ class TC_StringState < Test::Unit::TestCase
   public
   def test_marshal
     question = Mock.new( 'lucie/overview' )
-    question.__next( :name ) do || 'lucie/overview' end 
-    question.__next( :next_question ) do || 'lucie/caution' end       
-    line = Deft::StringState::marshal( question ).split("\n")
-    assert_match /class Lucie__Overview < Deft::StringState/, line[0]
+    question.__next( :state_class_name ) do || 'Deft::State::Lucie__Overview' end 
+    question.__next( :next_question ) do || 'lucie/caution' end   
+    question.__next( :next_question ) do || 'lucie/caution' end     
+    line = Deft::StringState::marshal_concrete_state( question ).split("\n")
+    assert_match /class Deft::State::Lucie__Overview < Deft::StringState/, line[0]
     assert_match /public/, line[1]
     assert_match /def transit\( aDebconfContext \)/, line[2]
     assert_match /super aDebconfContext/, line[3]
-    assert_match /aDebconfContext.current_state = DebconfContext::STATES\['lucie\/caution'\]/, line[4]
-    assert_match /end/, line[5]
+    assert_match /aDebconfContext.current_question = Deft::Question\['lucie\/caution'\]/, line[4]
+    assert_match /aDebconfContext.current_state\s*= Deft::ConcreteState\['lucie\/caution'\]/, line[5]
     assert_match /end/, line[6]
+    assert_match /end/, line[7]
     question.__verify
   end
 end

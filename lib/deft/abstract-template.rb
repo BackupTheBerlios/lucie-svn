03f8d88157e890756ca510d287cd673058a59741
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-20 13:11:43 +0000

    追加

---
+++ lib/deft/abstract-template.rb
@@ -0,0 +1,57 @@
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision$
+# License::  GPL2
+
+require 'forwardable'
+require 'time-stamp'
+
+update(%q$LastChangedDate$)
+
+module Deft
+  # すべてのテンプレートの親クラス
+  class AbstractTemplate
+    extend Forwardable
+    
+    def_delegator :@template, :name
+    def_delegator :@template, :template_type
+    def_delegator :@template, :default
+    def_delegator :@template, :choices
+    def_delegator :@template, :short_description
+    def_delegator :@template, :extended_description
+    def_delegator :@template, :short_description_ja
+    def_delegator :@template, :extended_description_ja
+    
+    # 新しい abstractTemplate オブジェクトを返す
+    public
+    def initialize( aTemplate )
+      @template = aTemplate
+    end
+    
+    # 文字列に変換する
+    public
+    def to_s
+      raise NotImplementedError, 'Abstract Method.'
+    end
+    
+    private
+    def template_string( typeString, *optionalFieldsArray )     
+      _template_string =  "Template: #{name}\n"
+      _template_string += "Type: #{typeString}\n"
+      _template_string += "Choices: #{choices.join(', ')}\n" if optionalFieldsArray.include?( 'choices' ) && choices
+      _template_string += "Default: #{default}\n" if optionalFieldsArray.include?( 'default' ) && default
+      _template_string += "Description: #{short_description}\n" if short_description
+      _template_string += extended_description.to_rfc822 + "\n" if extended_description
+      _template_string += "Description-ja: #{short_description_ja}\n" if short_description_ja
+      _template_string += extended_description_ja.to_rfc822 if extended_description_ja
+      return _template_string
+    end
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:
---
+++ test/deft/tc_abstract-template.rb
@@ -0,0 +1,88 @@
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision$
+# License::  GPL2
+
+$LOAD_PATH.unshift './lib'
+
+require 'mock'
+require 'deft/abstract-template'
+require 'test/unit'
+
+class TC_AbstractTemplate < Test::Unit::TestCase
+  public
+  def setup
+    @template = Mock.new( '[TEMPLATE]' )
+    @abstract_template = Deft::AbstractTemplate.new( @template )
+  end
+  
+  public
+  def test_name
+    @template.__next( :name ) do 'NAME' end
+    assert_equal 'NAME', @abstract_template.name
+    @template.__verify
+  end
+  
+  public
+  def test_template_type
+    @template.__next( :template_type ) do 'TEMPLATE TYPE' end
+    assert_equal 'TEMPLATE TYPE', @abstract_template.template_type
+    @template.__verify
+  end
+  
+  public
+  def test_default
+    @template.__next( :default ) do 'DEFAULT' end
+    assert_equal 'DEFAULT', @abstract_template.default
+    @template.__verify
+  end
+  
+  public
+  def test_choices
+    @template.__next( :choices ) do ['CHOICE 1', 'CHOICE 2', 'CHOICE 3'] end
+    assert_equal ['CHOICE 1', 'CHOICE 2', 'CHOICE 3'], @abstract_template.choices
+    @template.__verify
+  end
+  
+  public
+  def test_short_description
+    @template.__next( :short_description ) do 'SHORT DESCRIPTION' end
+    assert_equal 'SHORT DESCRIPTION', @abstract_template.short_description
+    @template.__verify
+  end
+  
+  public
+  def test_extended_description
+    @template.__next( :extended_description ) do 'EXTENDED DESCRIPTION' end
+    assert_equal 'EXTENDED DESCRIPTION', @abstract_template.extended_description
+    @template.__verify
+  end
+  
+  public
+  def test_short_description_ja
+    @template.__next( :short_description_ja ) do 'SHORT DESCRIPTION JA' end
+    assert_equal 'SHORT DESCRIPTION JA', @abstract_template.short_description_ja
+    @template.__verify
+  end
+  
+  public
+  def test_extended_description_ja
+    @template.__next( :extended_description_ja ) do 'EXTENDED DESCRIPTION JA' end
+    assert_equal 'EXTENDED DESCRIPTION JA', @abstract_template.extended_description_ja
+    @template.__verify
+  end
+  
+  public
+  def test_to_s
+    assert_raises( NotImplementedError, 'NotImplementedError ‚ª raise ‚³‚ê‚È‚©‚Á‚½' ) do
+      @abstract_template.to_s
+    end
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

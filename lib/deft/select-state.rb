09a7f56aa198473402ba679d675879ae6863f7a0
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-20 14:17:19 +0000

    リファクタリング

--- lib/deft/select-state.rb
+++ lib/deft/select-state.rb
@@ -5,24 +5,24 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-require 'lucie/state'
+require 'deft/state'
 require 'lucie/string'
-require 'lucie/time-stamp'
+require 'time-stamp'
+
+update(%q$Date$)
 
 module Deft
-  
-  Lucie.update(%q$Date$)
-  
   # select 型テンプレート変数の質問が表示されている状態を表すクラス
   class SelectState < State
     # Question オブジェクトから対応する SelectState クラスの子クラスをあらわす文字列を返す
-    def self.marshal( aQuestion )  
+    def self.marshal_concrete_state( aQuestion )  
       return ( <<-CLASS_DEFINITION ).unindent_auto
-      class #{aQuestion.name.to_state_class_name} < Deft::SelectState
+      class #{aQuestion.state_class_name} < Deft::SelectState
         public
         def transit( aDebconfContext )
           super aDebconfContext
-          aDebconfContext.current_state = DebconfContext::STATES['#{aQuestion.next_question}']
+          aDebconfContext.current_question = Deft::Question['#{aQuestion.next_question}']
+          aDebconfContext.current_state    = Deft::ConcreteState['#{aQuestion.next_question}']
         end
       end
       CLASS_DEFINITION
--- test/deft/tc_select-state.rb
+++ test/deft/tc_select-state.rb
@@ -14,27 +14,30 @@ require 'test/unit'
 class TC_SelectState < Test::Unit::TestCase
   # 以下のようなクラスをあらわす文字列が返されることを確認
   #
-  #  class LucieVmsetup__UseNetwork < Deft::SelectState
+  #  class Deft::State::LucieVmsetup__VmType < Deft::SelectState
   #    public
   #    def transit( aDebconfContext )
   #      super aDebconfContext
-  #      aDebconfContext.current_state = DebconfContext::STATES['lucie-vmsetup/distro']
+  #      aDebconfContext.current_question = Deft::Question['lucie-vmsetup/distro']
+  #      aDebconfContext.current_state    = DebconfContext::STATES['lucie-vmsetup/distro']
   #    end
   #  end
   public
   def test_marshal
     question = Mock.new( 'lucie-vmsetup/vm-type' )
-    question.__next( :name ) do || 'lucie-vmsetup/vm-type' end
+    question.__next( :state_class_name ) do || 'Deft::State::LucieVmsetup__VmType' end
+    question.__next( :next_question ) do || 'lucie-vmsetup/distro' end
     question.__next( :next_question ) do || 'lucie-vmsetup/distro' end 
     
-    line = Deft::SelectState::marshal( question ).split("\n")
-    assert_match /class LucieVmsetup__VmType < Deft::SelectState/, line[0]
+    line = Deft::SelectState::marshal_concrete_state( question ).split("\n")
+    assert_match /class Deft::State::LucieVmsetup__VmType < Deft::SelectState/, line[0]
     assert_match /public/, line[1]
     assert_match /def transit\( aDebconfContext \)/, line[2]
     assert_match /super aDebconfContext/, line[3]
-    assert_match /aDebconfContext.current_state = DebconfContext::STATES\['lucie-vmsetup\/distro'\]/, line[4]
-    assert_match /end/, line[5]
+    assert_match /aDebconfContext.current_question = Deft::Question\['lucie-vmsetup\/distro'\]/, line[4]
+    assert_match /aDebconfContext.current_state\s*= Deft::ConcreteState\['lucie-vmsetup\/distro'\]/, line[5]
     assert_match /end/, line[6]
+    assert_match /end/, line[7]
     question.__verify
   end
 end

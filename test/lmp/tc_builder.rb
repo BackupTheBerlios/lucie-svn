0accb4c7a3b6c7a7e882d5a48204e42fb3de0e16
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-22 07:48:00 +0000

    debian/control を生成する部分を修正

--- test/lmp/tc_builder.rb
+++ test/lmp/tc_builder.rb
@@ -28,7 +28,56 @@ lmp-test (0.1-1) unstable; urgency=low
     CHANGELOG
     end
     spec.__next( :config ) do end
-    spec.__next( :control ) do end
+    spec.__next( :control ) do <<-CONTROL
+Source: lmp-test
+Priority: optional
+Section: misc
+Maintainer: Yasuhito TAKAMIYA <takamiya@matsulab.is.titech.ac.jp>
+Build-Depends: debhelper (>= 4.0.0)
+Standards-Version: 3.6.0
+
+Package: lmp-test
+Architecture: all
+Description: Lucie メタパッケージのテスト
+Included packages:
+  o autoconf - automatic configure script builder
+  o automake - A tool for generating GNU Standards-compliant Makefiles.
+  o autoproject - create a skeleton source package for a new program
+  o binutils - The GNU assembler, linker and binary utilities
+  o bison - A parser generator that is compatible with YACC.
+  o c2man - Graham Stoney's mechanized man page generator
+  o cflow - C function call hierarchy analyzer
+  o cpp - The GNU C preprocessor (cpp)
+  o cpp-2.95 - The GNU C preprocessor
+  o cpp-3.2 - The GNU C preprocessor
+  o cpp-3.3 - The GNU C preprocessor
+  o cutils - C source code utilities
+  o cvs  - Concurrent Versions System
+  o cxref - Generates latex and HTML documentation for C programs.
+  o flex - A fast lexical analyzer generator.
+  o g++ - The GNU C++ compiler
+  o g++-2.95 - The GNU C++ compiler
+  o g++-3.0 - The GNU C++ compiler.
+  o g++-3.3 - The GNU C++ compiler
+  o gcc - The GNU C compiler
+  o gcc-2.95 - The GNU C compiler
+  o gcc-3.0 - The GNU C compiler.
+  o gcc-3.3 - The GNU C compiler
+  o gdb - The GNU Debugger
+  o gettext - GNU Internationalization utilities
+  o glibc-doc - GNU C Library: Documentation
+  o indent - C language source code formatting program
+  o libtool - Generic library support script
+  o liwc - Tools for manipulating C source code
+  o ltrace - Tracks runtime library calls in dynamically linked programs
+  o make  - The GNU version of the "make" utility.
+  o manpages-dev - Manual pages about using GNU/Linux for development
+  o nowebm - A WEB-like literate-programming tool.
+  o patch - Apply a diff file to an original
+  o stl-manual - C++-STL documentation in HTML
+  o strace - A system call tracer
+    CONTROL
+    end
     spec.__next( :copyright ) do end
     spec.__next( :postinst ) do end
     spec.__next( :rules ) do end

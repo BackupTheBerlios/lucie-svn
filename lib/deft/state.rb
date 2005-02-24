a6a0a1356d07153212e464ec294d259327e7fd8a
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-24 08:06:48 +0000

    state.rb に統合

--- lib/deft/state.rb
+++ lib/deft/state.rb
@@ -29,7 +29,32 @@ module Deft
     # +aQuestion+ を表す concrete class の Ruby スクリプトを文字列で返す
     public
     def self.marshal_concrete_state( aQuestion )
-      raise NotImplementedError, 'abstract method'
+      next_question = eval( aQuestion.next_question )
+      case next_question
+      when String
+        current_state = "Deft::ConcreteState[next_question]"
+      when Hash
+        current_state = "Deft::ConcreteState[next_question[get('#{aQuestion.name}')]]"
+      when Proc
+        current_state = "Deft::ConcreteState[next_question.call(get('#{aQuestion.name}'))]"
+      else
+        raise "Unsupported next_question type: #{next_question.class}"
+      end
+      
+      return ( <<-CLASS_DEFINITION ).unindent_auto
+      class #{aQuestion.state_class_name} < Deft::State
+        public
+        def transit( aDebconfContext )
+          super aDebconfContext
+          aDebconfContext.current_state = #{current_state}
+        end
+        
+        private
+        def next_question
+   #{('return ' + aQuestion.next_question.lstrip.rstrip).indent(6)}
+        end
+      end
+      CLASS_DEFINITION
     end
   end
 end

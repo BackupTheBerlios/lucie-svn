25f4b9d5be5a4ea95110de99fbd014640c15a43f
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-24 09:23:27 +0000

    リファクタリング

--- lib/deft/state.rb
+++ lib/deft/state.rb
@@ -5,6 +5,7 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
+require 'deft/concrete-state'
 require 'debconf/client'
 require 'time-stamp'
 require 'singleton'
@@ -19,17 +20,49 @@ module Deft
   class State    
     include Singleton
     
+    attr_reader :name
+    attr_reader :priority
+    attr_reader :first_state
+    
+    # 名前、プライオリティ、最初の State であるかどうかをセットし、
+    # ConcreteState として登録する。
+    public
+    def enhance( nameString, priorityString, firstState, rubyString )
+      @name = nameString
+      @priority = priorityString
+      @first_state = firstState
+      @ruby_string = rubyString
+      ConcreteState[@name] = self
+    end
+    
+    public
+    def to_ruby
+      return @ruby_string
+    end
+       
     # 次の State に遷移する
     public
     def transit( aDebconfContext )   
-      input aDebconfContext.current_question.priority, aDebconfContext.current_question.name
+      input aDebconfContext.current_state.priority, aDebconfContext.current_state.name
       go
     end
     
     # +aQuestion+ を表す concrete class の Ruby スクリプトを文字列で返す
     public
     def self.marshal_concrete_state( aQuestion )
-      next_question = eval( aQuestion.next_question )
+      if aQuestion.next_question.nil?
+        return ( <<-CLASS_DEFINITION ).unindent_auto
+        class #{aQuestion.state_class_name} < Deft::State
+          public
+          def transit( aDebconfContext )
+            super aDebconfContext
+            aDebconfContext.current_state = nil
+          end
+        end
+        CLASS_DEFINITION
+      end
+      
+      next_question = eval( aQuestion.next_question ) 
       case next_question
       when String
         current_state = "Deft::ConcreteState[next_question]"

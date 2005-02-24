732f81eaf08417ff5cc37804c5c98bbb4eb22321
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-24 13:41:37 +0000

    リファクタリング

--- lib/deft/concrete-state.rb
+++ lib/deft/concrete-state.rb
@@ -5,37 +5,34 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-require 'deft/question'
 require 'time-stamp'
 
 update(%q$Date$)
 
 module Deft
-  # concrete state を管理するクラス。
+  # Concrete State を管理するクラス。
   class ConcreteState
     STATES = {}
     
+    @@first_state = nil
+    
     # 登録されている Concrete State のリストを返す
-    public
     def self.states
       return STATES.values
     end
     
     # 登録されている Concrete State オブジェクトを名前で検索する
-    public
     def self.[]( stateNameString )
       return STATES[stateNameString]
     end
     
     # Concrete State を名前で登録する
-    public
     def self.[]=( stateNameString, concreteState )
       STATES[stateNameString] = concreteState
       @@first_state = concreteState if concreteState.first_state
     end
     
     # 最初の Concrete State を返す
-    public
     def self.first_state
       return @@first_state
     end

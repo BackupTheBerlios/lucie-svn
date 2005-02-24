00686028832392f427642df4ddfb5ce6559d0789
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-24 07:49:37 +0000

    debian/config をちゃんと生成できるようにリファクタリング

--- lib/deft/boolean-state.rb
+++ lib/deft/boolean-state.rb
@@ -15,15 +15,30 @@ module Deft
   # boolean 型テンプレート変数の質問が表示されている状態を表すクラス
   class BooleanState < State
     # Question オブジェクトから対応する BooleanState クラスの子クラスをあらわす文字列を返す
-    def self.marshal_concrete_state( aQuestion )  
+    def self.marshal_concrete_state( aQuestion )
+      next_question = eval( aQuestion.next_question )
+      case next_question
+      when String
+        current_state = "Deft::ConcreteState[next_question]"
+      when Hash
+        current_state = "Deft::ConcreteState[next_question[get('#{aQuestion.name}')]]"
+      when Proc
+        current_state = "Deft::ConcreteState[next_question.call(get('#{aQuestion.name}'))]"
+      else
+        raise "Unsupported next_question type: #{next_question.class}"
+      end
+      
       return ( <<-CLASS_DEFINITION ).unindent_auto
       class #{aQuestion.state_class_name} < Deft::BooleanState
         public
         def transit( aDebconfContext )
           super aDebconfContext
-          next_question = aDebconfContext.next_question(get( '#{aQuestion.name}' ))
-          aDebconfContext.current_question = Deft::Question[next_question]
-          aDebconfContext.current_state    = Deft::ConcreteState[next_question]
+          aDebconfContext.current_state = #{current_state}
+        end
+        
+        private
+        def next_question
+   #{('return ' + aQuestion.next_question.lstrip.rstrip).indent(6)}
         end
       end
       CLASS_DEFINITION
--- test/deft/tc_boolean-state.rb
+++ test/deft/tc_boolean-state.rb
@@ -7,28 +7,153 @@
 
 $LOAD_PATH.unshift './lib'
 
+require 'deft/concrete-state'
+require 'deft/question'
 require 'deft/boolean-state'
 require 'mock'
 require 'test/unit'
 
-class TC_NoteState < Test::Unit::TestCase
+class TC_BooleanState < Test::Unit::TestCase 
+  # next_question が String オブジェクトのときをテスト
   public
-  def test_marshal
-    question = Mock.new( 'lucie-vmsetup/use-network' )
-    question.__next( :state_class_name ) do || 'Deft::State::LucieVmsetup__UseNetwork' end
-    question.__next( :name ) do || 'lucie-vmsetup/use-network' end
+  def test_marshal_next_string   
+    question = Mock.new( '#<Question (Mock)>' )
+    question.__next( :next_question ) do "'lucie-vmsetup/next'" end  
+    question.__next( :state_class_name ) do 'Deft::State::LucieVmsetup__UseNetwork' end
+    question.__next( :next_question ) do "'lucie-vmsetup/next'" end 
+    
+    eval list = Deft::BooleanState::marshal_concrete_state( question )
+
+    debconf_context = Mock.new( '#<DebconfContext (Mock)>' )
+    current_question = Mock.new( '#<Question (Mock)>' )
+    debconf_context.__next( :current_question ) do current_question end
+    debconf_context.__next( :current_question ) do current_question end
+    debconf_context.__next( :current_state= ) do end
+    current_question.__next( :priority ) do Deft::Question::PRIORITY_MEDIUM end
+    current_question.__next( :name ) do 'lucie-vmsetup/use-network' end
+    
+    # -> INPUT medium lucie-vmsetup/use-network\n
+    # <- 0
+    # -> GO\n
+    # <- 0
+    $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
+    $stdin_mock = Mock.new( '#<STDIN (Mock)>' )    
+    $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "INPUT medium lucie-vmsetup/use-network\n", command )
+    end
+    $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "GO\n", command )
+    end
+    $stdin_mock.__next( :gets ) do '0' end
+    $stdin_mock.__next( :gets ) do '0' end
+    Deft::State::LucieVmsetup__UseNetwork.instance.transit( debconf_context )    
+    question.__verify    
+    $stdout_mock.__verify
+    $stdin_mock.__verify
+  end
   
-    line = Deft::BooleanState::marshal_concrete_state( question ).split("\n")
-    assert_match /class Deft::State::LucieVmsetup__UseNetwork < Deft::BooleanState/, line[0]
-    assert_match /public/, line[1]
-    assert_match /def transit\( aDebconfContext \)/, line[2]
-    assert_match /super aDebconfContext/, line[3]
-    assert_match /next_question = aDebconfContext\.next_question\[get\( 'lucie-vmsetup\/use-network' \)\]/, line[4]
-    assert_match /aDebconfContext\.current_question\s*= Deft::Question\[next_question\]/, line[5]
-    assert_match /aDebconfContext\.current_state\s*= Deft::ConcreteState\[next_question\]/, line[6]
-    assert_match /end/, line[7]
-    assert_match /end/, line[8]
+  # next_question が Hash オブジェクトのときをテスト
+  public
+  def test_marshal_next_hash
+    question = Mock.new( '#<Question (Mock)>' )
+    question.__next( :next_question ) do "{ 'true' => 'lucie-vmsetup/true', 'false' => 'lucie-vmsetup/false' }" end  
+    question.__next( :name ) do 'lucie-vmsetup/use-network' end   
+    question.__next( :state_class_name ) do 'Deft::State::LucieVmsetup__UseNetwork' end
+    question.__next( :next_question ) do "{ 'true' => 'lucie-vmsetup/true', 'false' => 'lucie-vmsetup/false' }" end  
+    
+    eval list = Deft::BooleanState::marshal_concrete_state( question ) 
+    
+    debconf_context = Mock.new( '#<DebconfContext (Mock)>' )
+    current_question = Mock.new( '#<Question (Mock)>' )
+    debconf_context.__next( :current_question ) do current_question end
+    debconf_context.__next( :current_question ) do current_question end
+    debconf_context.__next( :current_state= ) do end
+    current_question.__next( :priority ) do Deft::Question::PRIORITY_MEDIUM end
+    current_question.__next( :name ) do 'lucie-vmsetup/use-network' end
+    
+    # -> INPUT medium lucie-vmsetup/use-network\n
+    # <- 0
+    # -> GO\n
+    # <- 0
+    # -> GET lucie-vmsetup/use-network\n
+    # <- 0
+    $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
+    $stdin_mock = Mock.new( '#<STDIN (Mock)>' )    
+    $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "INPUT medium lucie-vmsetup/use-network\n", command )
+    end
+    $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "GO\n", command )
+    end
+    $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "GET lucie-vmsetup/use-network\n", command )
+    end
+    $stdin_mock.__next( :gets ) do '0' end
+    $stdin_mock.__next( :gets ) do '0' end
+    $stdin_mock.__next( :gets ) do '0' end
+    
+    Deft::State::LucieVmsetup__UseNetwork.instance.transit( debconf_context )    
     question.__verify
+    $stdout_mock.__verify
+    $stdin_mock.__verify
+  end
+  
+  # next_question が Proc オブジェクトのときをテスト
+  public
+  def test_marshal_next_proc    
+    next_question = <<-NEXT_QUESTION
+    Proc.new do |user_input|
+      case user_input
+      when 'debian (woody)', 'debian (sarge)'
+        'lucie-vmsetup/debian'
+      when 'redhat7.3'
+        'lucie-vmsetup/redhat'
+      end
+    end
+    NEXT_QUESTION
+    
+    question = Mock.new( '#<Question (Mock)>' )
+    question.__next( :next_question ) do next_question end  
+    question.__next( :name ) do 'lucie-vmsetup/use-network' end   
+    question.__next( :state_class_name ) do 'Deft::State::LucieVmsetup__UseNetwork' end
+    question.__next( :next_question ) do next_question end  
+    
+    eval list = Deft::BooleanState::marshal_concrete_state( question )
+    
+    # -> INPUT medium lucie-vmsetup/use-network\n
+    # <- 0
+    # -> GO\n
+    # <- 0
+    # -> GET lucie-vmsetup/use-network\n
+    # <- 0
+    debconf_context = Mock.new( '#<DebconfContext (Mock)>' )
+    current_question = Mock.new( '#<Question (Mock)>' )
+    debconf_context.__next( :current_question ) do current_question end
+    debconf_context.__next( :current_question ) do current_question end
+    debconf_context.__next( :current_state= ) do end
+    current_question.__next( :priority ) do Deft::Question::PRIORITY_MEDIUM end
+    current_question.__next( :name ) do 'lucie-vmsetup/use-network' end
+    
+    $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
+    $stdin_mock = Mock.new( '#<STDIN (Mock)>' )
+    
+    $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "INPUT medium lucie-vmsetup/use-network\n", command )
+    end
+    $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "GO\n", command )
+    end
+        $stdout_mock.__next( :print ) do |command| 
+      assert_equal( "GET lucie-vmsetup/use-network\n", command )
+    end
+    $stdin_mock.__next( :gets ) do '0' end
+    $stdin_mock.__next( :gets ) do '0' end
+    $stdin_mock.__next( :gets ) do '0' end
+    
+    Deft::State::LucieVmsetup__UseNetwork.instance.transit( debconf_context )
+    question.__verify
+    $stdout_mock.__verify
+    $stdin_mock.__verify
   end
 end
 

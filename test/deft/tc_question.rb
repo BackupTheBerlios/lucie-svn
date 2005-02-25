505c0e178693ce27b08d63acf6f6db74d5bc644e
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-02-25 12:06:57 +0000

    リファクタリング

--- test/deft/tc_question.rb
+++ test/deft/tc_question.rb
@@ -11,11 +11,16 @@ require 'deft/question'
 require 'test/unit'
 
 class TC_Question < Test::Unit::TestCase
+  public
+  def setup
+    Deft::Question.clear
+  end
+  
   # @name ‚Ì getter ‚ðƒeƒXƒg
   public
   def test_name_getter
     question = Deft::Question.new( 'OVERVIEW' )
-    assert_equal 'OVERVIEW', question.name
+    assert_equal( 'OVERVIEW', question.name )
   end
   
   # “o˜^‚³‚ê‚Ä‚¢‚é Question ‚ª‹ó‚Ì‚Æ‚«‚ÉA
@@ -23,7 +28,8 @@ class TC_Question < Test::Unit::TestCase
   public
   def test_question_defined_fail
     Deft::Question.clear
-    assert_nil Deft::Question.question_defined?( 'NOT DEFINED QUESTION' ), '“o˜^‚³‚ê‚Ä‚¢‚È‚¢‚Í‚¸‚ÌŽ¿–â€–Ú‚ª‚ ‚é'
+    assert_nil( Deft::Question.question_defined?( 'NOT DEFINED QUESTION' ), 
+                '“o˜^‚³‚ê‚Ä‚¢‚È‚¢‚Í‚¸‚ÌŽ¿–â€–Ú‚ª‚ ‚é' )
   end
   
   # Question ‚ð“o˜^‚µAquestion_defined? ‚Å“o˜^‚ªŠm”F‚Å‚«‚é‚±‚Æ‚ðƒeƒXƒg
@@ -31,15 +37,15 @@ class TC_Question < Test::Unit::TestCase
   def test_template_defined_success
     Deft::Question.clear
     question( 'TEST/QUESTION' )
-    assert Deft::Question.question_defined?( 'TEST/QUESTION' ), 'Ž¿–â€–Ú‚ª“o˜^‚³‚ê‚Ä‚¢‚È‚¢'
+    assert( Deft::Question.question_defined?( 'TEST/QUESTION' ), 'Ž¿–â€–Ú‚ª“o˜^‚³‚ê‚Ä‚¢‚È‚¢' )
   end
   
   # lookup ‚ÌƒeƒXƒg
   public
   def test_lookup_unknown_question
     question = Deft::Question::lookup( 'UNKNOWN QUESTION' )
-    assert_kind_of Deft::Question, question
-    assert_equal 'UNKNOWN QUESTION', question.name
+    assert_kind_of( Deft::Question, question, 'question ‚ÌŒ^‚ªˆá‚¤' )
+    assert_equal( 'UNKNOWN QUESTION', question.name, 'question ‚Ì–¼‘O‚ªˆá‚¤' )
   end
   
   # lookup ‚ÌƒeƒXƒg (‚ ‚ç‚©‚¶‚ß“o˜^‚µ‚½ Question ‚ðƒ‹ƒbƒNƒAƒbƒv)
@@ -47,8 +53,8 @@ class TC_Question < Test::Unit::TestCase
   def test_lookup_known_question
     Deft::Question::QUESTIONS['KNOWN QUESTION'] = Deft::Question.new('KNOWN QUESTION')
     question = Deft::Question::lookup( 'KNOWN QUESTION' )
-    assert_kind_of Deft::Question, question
-    assert_equal 'KNOWN QUESTION', question.name
+    assert_kind_of( Deft::Question, question, 'question ‚ÌŒ^‚ªˆá‚¤' )
+    assert_equal( 'KNOWN QUESTION', question.name, 'question ‚Ì–¼‘O‚ªˆá‚¤' )
   end
   
   public
@@ -57,7 +63,7 @@ class TC_Question < Test::Unit::TestCase
     question.enhance do 
       # DO NOTHING
     end
-    assert_equal 1, question.actions.size
+    assert_equal( 1, question.actions.size, 'ƒAƒNƒVƒ‡ƒ“‚Ì”‚ªˆá‚¤' )
   end
 end
 

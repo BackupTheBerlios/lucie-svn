9c2e72f80e0fa5426b1e29f8c26b0140dcbfea7f
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-03-01 05:25:07 +0000

    追加

---
+++ sbin/lucievmd.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+#
+# $Id: deft.rb 341 2005-02-26 17:08:19Z takamiya $
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $Revision: 341 $
+# License::  GPL2
+
+require 'socket'
+require 'English'
+
+port = (ARGV[0] || 5555).to_i
+lucievmd = TCPServer.open( 'localhost', port )
+addr = lucievmd.addr
+addr.shift
+printf( "server is on %s\n", addr.join(":"))
+
+loop do 
+  Thread.start( lucievmd.accept ) do |socket|
+  print( socket, " is acepted\n" )
+  while socket.gets  
+    socket.write($LAST_READ_LINE)
+  end
+  print( socket, "is gone\n" )
+  socket.close
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

ea68fb4b82162b2022ab1b04f48800d1ca352114
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-03-08 07:14:30 +0000

    o 使っていないタスクを削除 o リファクタリング

--- Rakefile.rb
+++ Rakefile.rb
@@ -14,24 +14,28 @@ task :default => [:testall]
 
 # Test Tasks -------------------------------------------------------------------
 
+debconf_filelist = FileList['test/debconf/tc_*.rb']
+deft_filelist = FileList['test/deft/tc_*.rb']
+lmp_filelist = FileList['test/lmp/tc_*.rb']
+
 desc "Run all the unit tests."
 Rake::TestTask.new( :testall ) do |t|
-  t.test_files = FileList['test/tc_*.rb']
+  t.test_files = (debconf_filelist << deft_filelist << lmp_filelist)
   t.verbose = true
 end
 
-Rake::TestTask.new(:testtask) do |t|
-  t.test_files = FileList['test/tc_task_*.rb']
+Rake::TestTask.new( :test_debconf ) do |t|
+  t.test_files = debconf_filelist
   t.verbose = true
 end
 
 Rake::TestTask.new( :test_deft ) do |t|
-  t.test_files = FileList['test/deft/tc_*.rb']
+  t.test_files = deft_filelist
   t.verbose = true
 end
 
 Rake::TestTask.new( :test_lmp ) do |t|
-  t.test_files = FileList['test/lmp/tc_*.rb']
+  t.test_files = lmp_filelist
   t.verbose = true
 end
 
@@ -40,23 +44,13 @@ end
 Rake::RDocTask.new( :rdoc ) do |rdoc|
   rdoc.title = 'Lucie documentation'
   rdoc.rdoc_dir = 'doc'
-  rdoc.options << '--line-numbers' << '--inline-source' << '--charset' << 'sjis' << '--diagram'
-  rdoc.rdoc_files.include( 'lib/*.rb', 'lib/debconf/*.rb', 'lib/deft/*.rb', 'lib/lucie/*.rb', 'lib/lmp/*.rb', 'lib/lucie/rake/*.rb' )
-end
-
-# templates ‚Ì¶¬ -------------------------------------------------------------
-
-desc 'templates ‚Ì¶¬'
-task :templates do
-  $LOAD_PATH.unshift './lib'
-  $LOAD_PATH.unshift './data'
-  require 'lucie_vm_template'
-  File::open( 'templates', 'w' ) do |file|
-    Lucie::Template::TEMPLATES.values.each do |each|
-      file.puts each
-      file.puts
-    end
-  end
+  rdoc.options << '--line-numbers' 
+  rdoc.options << '--inline-source' 
+  rdoc.options << '--charset' << 'sjis' 
+  rdoc.options << '--diagram'
+  rdoc.rdoc_files.include( 'lib/*.rb', 'lib/debconf/*.rb', 'lib/deft/*.rb', 
+                           'lib/lucie/*.rb', 'lib/lmp/*.rb', 
+                           'lib/lucie/rake/*.rb' )
 end
 
 ### Local variables:

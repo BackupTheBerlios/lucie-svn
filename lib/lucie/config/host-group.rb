9a98dfc2577faf05dabb3be95f5ae22e532666be
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-03-23 14:49:10 +0000

    to_s メソッドを追加

--- lib/lucie/config/host-group.rb
+++ lib/lucie/config/host-group.rb
@@ -25,6 +25,15 @@ module Lucie
       required_attribute :name
       required_attribute :alias
       required_attribute :members
+      
+      public
+      def to_s
+        if @alias
+          return "#{@name} (#{@alias})"
+        else
+          return name
+        end
+      end
     end
   end
 end

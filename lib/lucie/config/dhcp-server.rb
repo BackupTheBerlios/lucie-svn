e55a6866623e10a59d426e99c0455245dac66919
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-03-23 14:58:15 +0000

    to_s メソッドを追加

--- lib/lucie/config/dhcp-server.rb
+++ lib/lucie/config/dhcp-server.rb
@@ -30,6 +30,15 @@ module Lucie
       required_attribute :subnet
       required_attribute :dns
       required_attribute :domain_name
+      
+      public
+      def to_s
+        if @alias
+          return "#{@name} (#{@alias})"
+        else
+          return name
+        end
+      end
     end
   end
 end

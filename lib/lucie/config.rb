3c7886a1c43668c4cc0c5d011093057ca0059dd7
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-03-23 16:00:56 +0000

    追加

---
+++ lib/lucie/config.rb
@@ -0,0 +1,17 @@
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $Revision$
+# License::  GPL2
+
+require 'lucie/config/dhcp-server'
+require 'lucie/config/host'
+require 'lucie/config/host-group'
+require 'lucie/config/installer'
+require 'lucie/config/package-server'
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

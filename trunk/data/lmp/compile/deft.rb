86ba04c1b7cc4bed43c7dcf181ab6a9fdc86bfa1
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-04-26 06:56:28 +0000

    $BDI2C(B

---
+++ trunk/data/lmp/compile/deft.rb
@@ -0,0 +1,29 @@
+#
+# $Id: lucie_vm_template.rb 395 2005-03-10 08:28:02Z takamiya $
+#
+# Author::   Yoshiaki Sakae (mailto:sakae@is.titech.ac.jp)
+# Revision:: $LastChangedRevision: 395 $
+# License::  GPL2
+
+require 'deft'
+include Deft
+
+# ------------------------- Welcome メッセージ
+
+template( 'lucie-client/compile/hello' ) do |template|
+  template.template_type = 'note'
+  template.short_description = 'Welcome to lmp-compile setup wizard.'
+  template.extended_description = <<-DESCRIPTION
+  This metapackage will setup packages for developing software.
+  DESCRIPTION
+end
+
+question( 'lucie-client/compile/hello' ) do |question|
+  question.priority = Question::PRIORITY_MEDIUM
+  question.first_question = true
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

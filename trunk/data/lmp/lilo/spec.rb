e338a733aada1fd3e6532dd9d12cc3c52e0b8694
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-04-26 09:08:08 +0000

    o extended description $B$rDI2C(B o rdoc $B$rDI2C(B

--- trunk/data/lmp/lilo/spec.rb
+++ trunk/data/lmp/lilo/spec.rb
@@ -1,6 +1,12 @@
 #
 # $Id: lucie_vm_template.rb 395 2005-03-10 08:28:02Z takamiya $
 #
+# lilo によるブート処理をセットアップするための Lucie メタパッケージ
+#
+#--
+# TODO: lilo 各種オプションを priority low な質問として追加
+#++
+#
 # Author::   Yasuhito TAKAMIYA (mailto:takamiya@matsulab.is.titech.ac.jp)
 # Revision:: $LastChangedRevision: 395 $
 # License::  GPL2
@@ -11,15 +17,12 @@ spec = LMP::Specification.new do |spec|
   spec.maintainer = 'Yasuhito TAKAMIYA <takamiya@matsulab.is.titech.ac.jp>'
   spec.short_description = '[Lucie Meta Package] lilo'
   spec.extended_description = <<-EXTENDED_DESCRIPTION
-  Included packages:
-
-   o FIXME
-
+A Lucie Meta Package which setups lilo.
   EXTENDED_DESCRIPTION
 end
 
 lmp_package_task = Rake::LMPPackageTask.new( spec ) do |pkg|
-  pkg.package_dir = 'test/lmp/build/lilo'
+  pkg.package_dir = 'data/lmp/lilo'
   pkg.need_deb = true
 end
 

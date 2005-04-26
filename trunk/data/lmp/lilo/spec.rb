4b3ca03bdc10cae7a53a1f7bfade6ce6ea177821
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-04-26 08:45:48 +0000

    * $BDI2C(B

---
+++ trunk/data/lmp/lilo/spec.rb
@@ -0,0 +1,29 @@
+#
+# $Id: lucie_vm_template.rb 395 2005-03-10 08:28:02Z takamiya $
+#
+# Author::   Yasuhito TAKAMIYA (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision: 395 $
+# License::  GPL2
+
+spec = LMP::Specification.new do |spec|
+  spec.name = "lmp-lilo"
+  spec.version = "0.0.1-1"
+  spec.maintainer = 'Yasuhito TAKAMIYA <takamiya@matsulab.is.titech.ac.jp>'
+  spec.short_description = '[Lucie Meta Package] lilo'
+  spec.extended_description = <<-EXTENDED_DESCRIPTION
+  Included packages:
+
+   o FIXME
+
+  EXTENDED_DESCRIPTION
+end
+
+lmp_package_task = Rake::LMPPackageTask.new( spec ) do |pkg|
+  pkg.package_dir = 'test/lmp/build/lilo'
+  pkg.need_deb = true
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

13954db25ee00c944d88d33b177ddc722823b723
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-04-28 07:27:54 +0000

    空のテストを追加

---
+++ trunk/test/lucie/tc_setup-harddisk.rb
@@ -0,0 +1,18 @@
+#
+# $Id: tc_nfsroot-task.rb 544 2005-04-11 08:27:42Z takamiya $
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision: 544 $
+# License::  GPL2
+
+$LOAD_PATH.unshift './lib'
+
+require 'test/unit'
+
+class TC_SetupHarddisk < Test::Unit::TestCase
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

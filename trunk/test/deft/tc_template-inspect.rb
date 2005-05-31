6fd0d539092d8f4486e7f6a447692c44c7903727
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-05-31 07:48:01 +0000

    o リファクタリング

--- trunk/test/deft/tc_template-inspect.rb
+++ trunk/test/deft/tc_template-inspect.rb
@@ -10,7 +10,7 @@ $LOAD_PATH.unshift './lib'
 require 'deft/template'
 require 'test/unit'
 
-# Šeƒeƒ“ƒvƒŒ[ƒgƒNƒ‰ƒX‚Ì inspect ƒƒ\ƒbƒh‚ðƒeƒXƒg
+# ³Æ¥Æ¥ó¥×¥ì¡¼¥È¥¯¥é¥¹¤Î inspect ¥á¥½¥Ã¥É¤ò¥Æ¥¹¥È
 class TC_TemplateInspect < Test::Unit::TestCase
   public
   def setup
@@ -24,65 +24,65 @@ class TC_TemplateInspect < Test::Unit::TestCase
   
   public
   def test_inspect_text_template
-    template = template( 'TEST TEMPLATE' ) do |template|
+    template = template( 'TEXT TEMPLATE' ) do |template|
       template.template_type = 'text'
     end
-    assert_equal( '#<Deft::TextTemplate: @name="TEST TEMPLATE">',
-                  template.inspect )
+    assert_equal( '#<Deft::TextTemplate: @name="TEXT TEMPLATE">',
+                  template.inspect, 'text ¥Æ¥ó¥×¥ì¡¼¥È¤Î inspect Ê¸»úÎó¤¬Àµ¤·¤¯¤Ê¤¤' )
   end
   
   public
   def test_inspect_select_template
-    template = template( 'TEST TEMPLATE' ) do |template|
+    template = template( 'SELECT TEMPLATE' ) do |template|
       template.template_type = 'select'
     end
-    assert_equal( '#<Deft::SelectTemplate: @name="TEST TEMPLATE">',
-                  template.inspect )
+    assert_equal( '#<Deft::SelectTemplate: @name="SELECT TEMPLATE">',
+                  template.inspect, 'select ¥Æ¥ó¥×¥ì¡¼¥È¤Î inspect Ê¸»úÎó¤¬Àµ¤·¤¯¤Ê¤¤' )
   end
   
   public
   def test_inspect_note_template
-    template = template( 'TEST TEMPLATE' ) do |template|
+    template = template( 'NOTE TEMPLATE' ) do |template|
       template.template_type = 'note'
     end
-    assert_equal( '#<Deft::NoteTemplate: @name="TEST TEMPLATE">',
-                  template.inspect )
+    assert_equal( '#<Deft::NoteTemplate: @name="NOTE TEMPLATE">',
+                  template.inspect, 'note ¥Æ¥ó¥×¥ì¡¼¥È¤Î inspect Ê¸»úÎó¤¬Àµ¤·¤¯¤Ê¤¤' )
   end
   
   public
   def test_inspect_boolean_template
-    template = template( 'TEST TEMPLATE' ) do |template|
+    template = template( 'BOOLEAN TEMPLATE' ) do |template|
       template.template_type = 'boolean'
     end
-    assert_equal( '#<Deft::BooleanTemplate: @name="TEST TEMPLATE">',
-                  template.inspect )
+    assert_equal( '#<Deft::BooleanTemplate: @name="BOOLEAN TEMPLATE">',
+                  template.inspect, 'boolean ¥Æ¥ó¥×¥ì¡¼¥È¤Î inspect Ê¸»úÎó¤¬Àµ¤·¤¯¤Ê¤¤' )
   end
 
   public
   def test_inspect_string_template
-    template = template( 'TEST TEMPLATE' ) do |template|
+    template = template( 'STRING TEMPLATE' ) do |template|
       template.template_type = 'string'
     end
-    assert_equal( '#<Deft::StringTemplate: @name="TEST TEMPLATE">',
-                  template.inspect )
+    assert_equal( '#<Deft::StringTemplate: @name="STRING TEMPLATE">',
+                  template.inspect, 'string ¥Æ¥ó¥×¥ì¡¼¥È¤Î inspect Ê¸»úÎó¤¬Àµ¤·¤¯¤Ê¤¤' )
   end  
   
   public
   def test_inspect_multiselect_template
-    template = template( 'TEST TEMPLATE' ) do |template|
+    template = template( 'MULTISELECT TEMPLATE' ) do |template|
       template.template_type = 'multiselect'
     end
-    assert_equal( '#<Deft::MultiselectTemplate: @name="TEST TEMPLATE">',
-                  template.inspect )
+    assert_equal( '#<Deft::MultiselectTemplate: @name="MULTISELECT TEMPLATE">',
+                  template.inspect, 'multiselect ¥Æ¥ó¥×¥ì¡¼¥È¤Î inspect Ê¸»úÎó¤¬Àµ¤·¤¯¤Ê¤¤' )
   end
   
   public
   def test_inspect_password_template
-    template = template( 'TEST TEMPLATE' ) do |template|
+    template = template( 'PASSWORD TEMPLATE' ) do |template|
       template.template_type = 'password'
     end
-    assert_equal( '#<Deft::PasswordTemplate: @name="TEST TEMPLATE">',
-                  template.inspect )
+    assert_equal( '#<Deft::PasswordTemplate: @name="PASSWORD TEMPLATE">',
+                  template.inspect, 'password ¥Æ¥ó¥×¥ì¡¼¥È¤Î inspect Ê¸»úÎó¤¬Àµ¤·¤¯¤Ê¤¤' )
   end        
 end
 

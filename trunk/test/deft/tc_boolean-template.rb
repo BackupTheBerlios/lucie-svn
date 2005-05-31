768dc354ece883b851c37a45fa2c8e78d813af66
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-05-31 09:55:09 +0000

    o to_s のテストを test/deft/tc_template-to_s.rb に集めた

--- trunk/test/deft/tc_boolean-template.rb
+++ trunk/test/deft/tc_boolean-template.rb
@@ -68,28 +68,6 @@ class TC_BooleanTemplate < Test::Unit::TestCase
     end
   end
   
-  # to_s で templates 文字列が正しく生成されることをテスト
-  #--
-  # TODO: default= の引数チェック (yes/no だけ受け付ける)
-  #++ 
-  public
-  def test_to_s
-    @boolean_template.default = 'yes'
-    @boolean_template.short_description = 'TEST SHORT DESCRIPTION'
-    @boolean_template.extended_description = 'TEST EXTENDED DESCRIPTION'
-    @boolean_template.short_description_ja = 'TEST SHORT DESCRIPTION JA'
-    @boolean_template.extended_description_ja = 'TEST EXTENDED DESCRIPTION JA'
-    assert_equal( (<<-BOOLEAN_TEMPLATE).chomp, @boolean_template.to_s, "to_s が正しい値を返さない" )
-Template: TEST BOOLEAN TEMPLATE
-Type: boolean
-Default: yes
-Description: TEST SHORT DESCRIPTION
- TEST EXTENDED DESCRIPTION
-Description-ja: TEST SHORT DESCRIPTION JA
- TEST EXTENDED DESCRIPTION JA
-   BOOLEAN_TEMPLATE
-  end
-
   # @choices= が Deft::Exception::InvalidAttributeException を返すことをテスト
   public 
   def test_setter_choices_raises_invalid_attribute_exception

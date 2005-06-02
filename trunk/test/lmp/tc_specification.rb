444b37c4a5486c28fa546e2478a5022bded4a7f8
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-02 07:30:21 +0000

    o inspect と to_s のテストを追加

--- trunk/test/lmp/tc_specification.rb
+++ trunk/test/lmp/tc_specification.rb
@@ -7,9 +7,26 @@
 
 $LOAD_PATH.unshift './lib'
 
+require 'lmp/specification'
 require 'test/unit'
 
 class TC_Specification < Test::Unit::TestCase
+  public
+  def setup
+    @specification = LMP::Specification.new
+  end
+
+  public
+  def test_to_s
+    assert_equal( %{#<LMP::Specification name= version=>}, @specification.to_s,
+                  "Specification#to_s の返り値が正しくない" )
+  end
+
+  public
+  def test_inspect
+    assert_equal( %{#<LMP::Specification name= version=>}, @specification.inspect,
+                  "Specification#inspect の返り値が正しくない" )
+  end
 end
 
 ### Local variables:

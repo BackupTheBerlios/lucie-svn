0e7e020f92c41e917918b060230848c73c075156
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-02 07:46:08 +0000

    o テンプレート生成メソッドのテストを追加

--- trunk/test/lmp/tc_specification.rb
+++ trunk/test/lmp/tc_specification.rb
@@ -16,6 +16,49 @@ class TC_Specification < Test::Unit::TestCase
     @specification = LMP::Specification.new
   end
 
+  # ------------------------- Template メソッドのテスト.
+
+  # 生成されるテンプレートの内容については考えず、テンプレート生成メソッ
+  # ドがあるかどうかのみをテスト
+
+  public
+  def test_respond_to_config
+    assert( @specification.respond_to?( :config ),
+            "Specificatoin#config メソッドが無い" )
+  end
+
+  public
+  def test_respond_to_control
+    assert( @specification.respond_to?( :control ),
+            "Specificatoin#control メソッドが無い" )
+  end
+
+  public
+  def test_respond_to_rules
+    assert( @specification.respond_to?( :rules ),
+            "Specificatoin#rules メソッドが無い" )
+  end
+
+  public
+  def test_respond_to_readme
+    assert( @specification.respond_to?( :readme ),
+            "Specificatoin#readme メソッドが無い" )
+  end
+
+  public
+  def test_respond_to_changelog
+    assert( @specification.respond_to?( :changelog ),
+            "Specificatoin#changelog メソッドが無い" )
+  end
+
+  public
+  def test_respond_to_templates
+    assert( @specification.respond_to?( :templates ),
+            "Specificatoin#templates メソッドが無い" )
+  end
+
+  # ------------------------- Debug メソッドのテスト.
+
   public
   def test_to_s
     assert_equal( %{#<LMP::Specification name= version=>}, @specification.to_s,

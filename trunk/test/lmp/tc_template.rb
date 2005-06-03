1fcc32c4c91c5a65f94e80e86808d5fd51c65906
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-03 08:58:07 +0000

    o 追加

---
+++ trunk/test/lmp/tc_template.rb
@@ -0,0 +1,18 @@
+#
+# $Id$
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision$
+# License::  GPL2
+
+$LOAD_PATH.unshift './lib'
+
+require 'test/unit'
+
+class TC_Template < Test::Unit::TestCase
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

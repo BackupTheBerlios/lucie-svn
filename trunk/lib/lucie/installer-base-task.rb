396301f68c5678ae62c0a84710fc6a1e9970480b
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-06 08:46:18 +0000

    o kterm で Debconf を動かすため debootstrap でインストールされるパッケージに ncurses-term を追加

--- trunk/lib/lucie/installer-base-task.rb
+++ trunk/lib/lucie/installer-base-task.rb
@@ -9,58 +9,14 @@ require 'rake'
 require 'rake/tasklib'
 require 'lucie/command-line-options'
 
-Lucie::update(%q$Date$)
-
 module Rake
-  #
-  # ’¥¤’¥ó’¥¹’¥È’¡¼’¥é’¤Î’¥Ù’¡¼’¥¹’¥·’¥¹’¥Æ’¥à’¤ò’¥Ó’¥ë’¥É’¤¹’¤ë’¥¿’¥¹’¥¯’¤ò’ºî’À®’¤¹’¤ë’¡£
-  #
-  # InstallerBaseTask ’¤Ï’¼¡’¤Î’¥¿’¡¼’¥²’¥Ã’¥È’¤ò’ºî’À®’¤¹’¤ë:
-  #
-  # [<b><em>installer_base</em></b>]
-  #   InstallerBase ’¥¿’¥¹’¥¯’¤Î’¥á’¥¤’¥ó’¥¿’¥¹’¥¯
-  # [<b><em>:clobber_installer_base</em></b>]
-  #   ’¤¹’¤Ù’¤Æ’¤Î’¥¤’¥ó’¥¹’¥È’¡¼’¥é’¥Ù’¡¼’¥¹’´Ø’Ï¢’¥Õ’¥¡’¥¤’¥ë’¤ò’¾Ã’µî’¤¹’¤ë’¡£
-  #   ’¤³’¤Î’¥¿’¡¼’¥²’¥Ã’¥È’¤Ï’¼«’Æ°’Åª’¤Ë’¥á’¥¤’¥ó’¤Î clobber ’¥¿’¡¼’¥²’¥Ã’¥È’¤Ë’ÄÉ’²Ã’¤µ’¤ì’¤ë
-  # [<b><em>:reinstaller_base</em></b>]
-  #   ’¥¿’¥¤’¥à’¥¹’¥¿’¥ó’¥×’¤Ë’´Ø’¤ï’¤é’¤º’¥¤’¥ó’¥¹’¥È’¡¼’¥é’¥Ù’¡¼’¥¹’¤ò’¤Þ’¤Ã’¤µ’¤é’¤«’¤é’¥ê’¥Ó’¥ë’¥É’¤¹’¤ë
-  #
-  # ’Îã:
-  #
-  #   InstallerBaseTask.new do |installer_base|
-  #     installer_base.dir = "tmp"
-  #     installer_base.mirror = "http://www.debian.or.jp/debian"
-  #     installer_base.distribution = "debian"
-  #     installer_base.distribution_version = "sarge"
-  #   end
-  #
-  # ’ºî’À®’¤¹’¤ë InstallerBaseTask ’¤Ë’¤Ï’¥Ç’¥Õ’¥©’¥ë’¥È’¤Î’Ì¾’Á°’°Ê’³°’¤Ë’¼«’Ê¬’¤Î’¹¥’¤­’¤Ê’Ì¾’Á°’¤ò
-  # ’¤Ä’¤±’¤ë’¤³’¤È’¤â’¤Ç’¤­’¤ë’¡£
-  #
-  #   InstallerBaseTask.new(:installer_base_woody) do |installer_base|
-  #     installer_base.dir = "tmp"
-  #     installer_base.mirror = "http://www.debian.or.jp/debian"
-  #     installer_base.distribution = "debian"
-  #     installer_base.distribution_version = "woody"
-  #   end
-  # 
-  # ’¤³’¤Î’¾ì’¹ç’¡¢<em>:installer_base_woody</em>, :clobber_<em>installer_base_woody</em>, 
-  # :re<em>installer_base_woody</em> ’¤È’¤¤’¤¦’Ì¾’Á°’¤Î’¥¿’¥¹’¥¯’¤¬’À¸’À®’¤µ’¤ì’¤ë’¡£
-  #
   class InstallerBaseTask < TaskLib
-    # ’¥¤’¥ó’¥¹’¥È’¡¼’¥é’¥Ù’¡¼’¥¹’ºî’À®’¥¿’¥¹’¥¯’¤Î’Ì¾’Á° (’¥Ç’¥Õ’¥©’¥ë’¥È: :installer_base )
     attr_accessor :name
-    # ’¥¤’¥ó’¥¹’¥È’¡¼’¥é’¥Ù’¡¼’¥¹’¤ò’ºî’À®’¤¹’¤ë’¥Ç’¥£’¥ì’¥¯’¥È’¥ê’¤Ø’¤Î’¥Ñ’¥¹ 
-    # (’¥Ç’¥Õ’¥©’¥ë’¥È: '/var/lib/lucie/installer-base/' )
     attr_accessor :dir
-    # Debian ’¤Î’¥ß’¥é’¡¼’¤Î URI (’¥Ç’¥Õ’¥©’¥ë’¥È: http://www.debian.or.jp/debian)
     attr_accessor :mirror
-    # ’¥¤’¥ó’¥¹’¥È’¡¼’¥é’¥Ù’¡¼’¥¹’¤Î’¥Ç’¥£’¥¹’¥È’¥ê’¥Ó’¥å’¡¼’¥·’¥ç’¥ó (’¥Ç’¥Õ’¥©’¥ë’¥È: nil)
     attr_accessor :distribution
-    # ’¥¤’¥ó’¥¹’¥È’¡¼’¥é’¥Ù’¡¼’¥¹’¤Î’¥Ç’¥£’¥¹’¥È’¥ê’¥Ó’¥å’¡¼’¥·’¥ç’¥ó’¤Î’¥Ð’¡¼’¥¸’¥ç’¥ó (’¥Ç’¥Õ’¥©’¥ë’¥È: nil)
     attr_accessor :distribution_version
     
-    # InstallerBase ’¥¿’¥¹’¥¯’¤ò’ºî’À®’¤¹’¤ë’¡£’¥Ç’¥Õ’¥©’¥ë’¥È’¤Î’Ì¾’Á°’¤Ï +installer_base+
     public
     def initialize( name=:installer_base ) # :yield: self
       @name = name
@@ -89,12 +45,10 @@ module Rake
       task name => [installer_base_target]
       
       file installer_base_target do
-        # TODO: debootstrap ’¤Î’¥ª’¥×’¥·’¥ç’¥ó’¤ò’»Ø’Äê’²Ä’Ç½’¤Ë’¤¹’¤ë
-        debootstrap_option = "--arch i386 --exclude=#{exclude_packages.join(',')}"
+        debootstrap_option = "--arch i386 --exclude=#{exclude_packages.join(',')} --include=ncurses-term"
         info "Executing debootstrap. This may take a long time."
         sh_log( %{yes '' | LC_ALL=C debootstrap #{debootstrap_option} #{@distribution_version} #{@dir} #{@mirror} 2>&1}, sh_option ) do |rd|
           line_length = 0
-          # TODO: ’¥×’¥í’¥°’¥ì’¥¹’¥Ð’¡¼’¤Ç’¿Ê’Ä½’¤ò’É½’¼¨
           while (rd.gets)
             line = $_.chomp
             case line

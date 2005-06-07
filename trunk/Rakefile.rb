bbf0bb19e70804a7483d04dc452f98cf683e3bc6
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-07 09:08:35 +0000

    network lmp を rdoc 生成対象に追加

--- trunk/Rakefile.rb
+++ trunk/Rakefile.rb
@@ -58,7 +58,7 @@ Rake::RDocTask.new( :rdoc ) do |rdoc|
   rdoc.options << '--diagram'
   rdoc.rdoc_files.include( 'lib/*.rb', 'lib/debconf/*.rb', 'lib/deft/*.rb', 
                            'lib/lucie/*.rb', 'lib/lucie/config/*.rb', 'lib/lmp/*.rb', 
-                           'data/lmp/compile/*.rb', 'data/lmp/lilo/*.rb',
+                           'data/lmp/compile/*.rb', 'data/lmp/lilo/*.rb', 'data/lmp/network/*.rb',
                            'lib/lucie/rake/*.rb' )
 end
 

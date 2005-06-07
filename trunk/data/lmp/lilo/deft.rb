a128b621140b16e9ee4dd62c024c0d149e04f0f6
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-07 08:16:57 +0000

    o libdevmapper のエラーについて追記

--- trunk/data/lmp/lilo/deft.rb
+++ trunk/data/lmp/lilo/deft.rb
@@ -14,8 +14,24 @@ template( 'lucie-client/lilo/hello' ) do |template|
   template.template_type = 'note'
   template.short_description = 'Welcome to lmp-lilo setup wizard.'
   template.extended_description = <<-DESCRIPTION
-  This metapackage will setup lilo configuration.
+  This metapackage will generate Lucie configuration of lilo.
+
+  NOTE: Lucie lilo script provided by this package may fail because of
+  the lack of necessary libdevmapper shared library in nfsroot. In
+  this case, please add libdevmapper1.01 (or 1.00) to the
+  installer.extra_packages in /etc/lucie/resource.rb and re-run
+  lucie-setup
   DESCRIPTION
+  template.short_description_ja = 'lmp-lilo セットアップウィザードへようこそ'
+  template.extended_description_ja = <<-DESCRIPTION_JA
+  このメタパッケージは lilo の設定を Lucie サーバへ行います。
+
+  注意: このメタパッケージによって提供される Lucie スクリプトは、
+  nfsroot 上に必要な libdevmapper 共有ライブラリがインストールされてい
+  ないために失敗する場合があります。この場合、/etc/lucie/resource.rb 
+  の installer.extra_packages に libdevmapper1.01 (もしくは 1.00) を追
+  加し、lucie-setup を再実行してください。
+  DESCRIPTION_JA
 end
 
 question( 'lucie-client/lilo/hello' ) do |question|

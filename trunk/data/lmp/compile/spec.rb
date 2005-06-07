edc4956be88c73476853ab64780b75ef216b05d0
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-07 07:22:35 +0000

    o バージョン 0.0.2-1

--- trunk/data/lmp/compile/spec.rb
+++ trunk/data/lmp/compile/spec.rb
@@ -4,18 +4,13 @@
 # コンパイル環境 (コンパイラ、デバッガ、各種ツール、ドキュメント類) を
 # セットアップするための Lucie メタパッケージ。
 #
-#--
-# TODO: コンパイラのバージョン選択を可能にする
-# TODO: /etc/alternatives の設定を自動的に行う
-#++
-#
 # Author::   Yasuhito TAKAMIYA (mailto:takamiya@matsulab.is.titech.ac.jp)
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
 spec = LMP::Specification.new do |spec|
   spec.name = "lmp-compile"
-  spec.version = "0.0.1-1"
+  spec.version = "0.0.2-1"
   spec.maintainer = 'Yasuhito TAKAMIYA <takamiya@matsulab.is.titech.ac.jp>'
   spec.short_description = '[Lucie Meta Package] compile'
   spec.extended_description = <<-EXTENDED_DESCRIPTION

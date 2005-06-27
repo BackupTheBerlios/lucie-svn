580156c32141fe81a39b388788062de81b6731fc
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-27 07:53:14 +0000

    o rdoc の修正 o 若干のリファクタリング

--- trunk/test/depends/tc_cache.rb
+++ trunk/test/depends/tc_cache.rb
@@ -4,47 +4,50 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-$LOAD_PATH.unshift '../../lib'
+$LOAD_PATH.unshift './lib'
 
 require 'depends'
 require 'test/unit'
 
 class TC_Cache < Test::Unit::TestCase
+  # テスト中に生成されたキャッシュファイルを消去
   public
   def teardown
     File.delete( cache_filepath ) if FileTest.file?( cache_filepath )
   end
 
+  # Cache クラス経由で Pool インスタンスを取得できることを確認
   public
-  def test_object_pool_cache
-    assert_kind_of Depends::Pool, get_pool # not cached
-    assert_kind_of Depends::Pool, get_pool # cached
+  def test_pool_cache
+    assert_kind_of( Depends::Pool, get_pool, 'Pool オブジェクトが Cache から取得できない' ) # not cached
+    assert_kind_of( Depends::Pool, get_pool, 'Pool オブジェクトが Cache から取得できない' ) # cached
   end
 
+  # inspect メソッドの結果をテスト
   public
   def test_inspect
     cache = Depends::Cache.new( cache_directory, cache_filename )
-    assert_equal "<Cache: cachefile='#{cache_filepath}'>", cache.inspect
+    assert_equal( %{<Cache: cachefile='#{cache_filepath}'>}, cache.inspect, 'inspect の返り値が正しくない' )
   end
   
   private
   def get_pool
-    Depends::Cache.new( cache_directory, cache_filename ).pool
+    return Depends::Cache.new( cache_directory, cache_filename ).pool
   end
 
   private 
   def cache_filepath
-    File.join( cache_directory, cache_filename )
+    return File.join( cache_directory, cache_filename )
   end
 
   private
   def cache_directory
-    '/tmp'
+    return '/tmp'
   end
 
   private
   def cache_filename
-    'tc_cache.cache'
+    return 'tc_cache.cache'
   end
 end
 

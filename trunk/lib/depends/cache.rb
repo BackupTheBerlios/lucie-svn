4e47804448c3e0418da8d3815c7bef632a5d8889
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-27 05:02:29 +0000

    o rdoc コメントを追加/修正 o メソッド抽出など若干のリファクタリング

--- trunk/lib/depends/cache.rb
+++ trunk/lib/depends/cache.rb
@@ -1,4 +1,4 @@
-# = cache.rb - Cache faculty class.
+# =パッケージ間依存情報のキャッシュ管理用クラス定義
 #
 # $Id: cache.rb,v 1.5 2004/06/30 06:50:19 takamiya Exp $
 #
@@ -7,74 +7,85 @@
 # License:: GPL2
 
 module Depends
-  # Cache class for dependency object pool.
+  # パッケージ間依存情報プール (Pool) のキャッシュ管理用クラス。Pool 
+  # の内容のキャッシングを自動的に行うため、生で Pool クラスを扱うのに
+  # 比べて高速化される。
+  # 
+  # _Example_
+  #
+  #  pool = Depends::Cache.new.pool
+  #
+  # キャッシュされている Pool オブジェクトがあればそれを使用し、無けれ
+  # ば新たに生成しキャッシュする。
+  #
   class Cache
+    # キャッシュ作成用ディレクトリ
     DEFAULT_CACHE_DIRECTORY = File.expand_path('/var/cache/depends/')
+    # キャッシュファイル名
     DEFAULT_CACHE_FILENAME = 'depends.cache'
-    attr :pool
+    # パッケージ間依存情報プール
+    attr_reader :pool
 
-    # Returns a new Cache object.
-    #
-    # _Example_:
-    #  Cache.new.pool #=> aNewPoolObj.
-    #
     def initialize( cacheDirectoryString=DEFAULT_CACHE_DIRECTORY, 
                     cacheFilenameString=DEFAULT_CACHE_FILENAME )
       @cache_directory = cacheDirectoryString
       @cache_filename = cacheFilenameString
-
-      # persist/restore object pool
       if cache_exists?
-	if cache_timestamp < status_timestamp
-	  STDERR.puts "cache is outdated, refreshing..."
-	  update_cache
-	else
-	  load_cache
-	end
+        restore_from_file
       else
-	STDERR.puts "cache does not exist, creating..."
+	STDERR.puts "cache does not exist, creating..." if $trace
 	update_cache
       end
     end
 
     public
     def inspect #:nodoc:
-      "<Cache: cachefile='#{cache_filepath}'>"
+      return %{<Cache: cachefile='#{cache_filepath}'>}
+    end
+
+    private
+    def restore_from_file
+      if cache_timestamp < status_timestamp
+        STDERR.puts "cache is outdated, refreshing..." if $trace
+        update_cache
+      else
+        load_cache
+      end
     end
 
     private
     def cache_exists?
-      test(?f, cache_filepath)
+      return FileTest.exist?( cache_filepath )
     end
 
     private
     def status_timestamp
-      File.stat( STATUS ).mtime
+      return File.stat( STATUS ).mtime
     end
 
     private
     def cache_timestamp
-      File.stat( cache_filepath ).mtime
+      return File.stat( cache_filepath ).mtime
     end
 
     private
     def cache_filepath
-      @cache_directory + '/' + @cache_filename
+      return File.join( @cache_directory, @cache_filename )
     end
     
     private
     def update_cache
-      @pool = Depends::Pool.new
-      File.open( cache_filepath, 'w' ){ |file|
+      @pool = Pool.new
+      File.open( cache_filepath, 'w' ) do |file|
 	Marshal.dump @pool, file
-      }
+      end
     end
 
     private
     def load_cache
-      File.open( cache_filepath, 'r' ){ |file|
+      File.open( cache_filepath, 'r' ) do |file|
 	@pool = Marshal.load( file )
-      }
+      end
     end
   end
 end 

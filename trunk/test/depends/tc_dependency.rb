4ea91049420bbf18a958fffa085f13b225eb1424
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-06-27 08:30:22 +0000

    o テストにメッセージを追加

--- trunk/test/depends/tc_dependency.rb
+++ trunk/test/depends/tc_dependency.rb
@@ -4,7 +4,7 @@
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-$LOAD_PATH.unshift '../../lib'
+$LOAD_PATH.unshift './lib'
 
 require 'depends'
 require 'test/unit'
@@ -14,37 +14,37 @@ class TestDependency < Test::Unit::TestCase
   public
   def test_relation_ge
     depends = Depends::Dependency.new( 'q-bert (>= 1.0)' )
-
-    assert_equal 'q-bert', depends.name
-    assert_equal '1.0', depends.version
+    
+    assert_equal( 'q-bert', depends.name,    'name 属性が正しくない' )
+    assert_equal( '1.0',    depends.version, 'version 属性が正しくない' )
 
     # == 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.0
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定: >=)' )
 
     # > 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.1
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call( package ), '依存関係が正しくない (バージョン指定: >=)' )
 
     # < 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 0.9
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call( package ), '依存関係が正しくない (バージョン指定: >=)' )
 
     # パッケージ名が異なる (fail)
     package = Depends::Package.new(<<STATUS)
 Package: d-styles
 Version: 1.0
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call( package ), '依存関係が正しくない (バージョン指定: >=)' )
   end
 
   # '>>' な relation のテスト
@@ -52,73 +52,73 @@ STATUS
   def test_relation_gt
     depends = Depends::Dependency.new( 'q-bert (>> 1.0)' )
 
-    assert_equal 'q-bert', depends.name
-    assert_equal '1.0', depends.version
+    assert_equal( 'q-bert', depends.name,    'name 属性が正しくない' )
+    assert_equal( '1.0',    depends.version, 'version 属性が正しくない' )
 
     # == 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.0
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: >>)' )
 
     # > 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.1
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定: >>)' )
 
     # < 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 0.9
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: >>)' )
 
     # パッケージ名が異なる (fail)
     package = Depends::Package.new(<<STATUS)
 Package: d-styles
 Version: 1.1
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: >>)' )
   end
 
   # '<=' な relation のテスト
   public
   def test_relation_le
     depends = Depends::Dependency.new( 'q-bert (<= 1.0)' )
-
-    assert_equal 'q-bert', depends.name
-    assert_equal '1.0', depends.version
+    
+    assert_equal( 'q-bert', depends.name,    'name 属性が正しくない' )
+    assert_equal( '1.0',    depends.version, 'version 属性が正しくない' )
 
     # == 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.0
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定: <=)' )
 
     # < 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 0.9
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定: <=)' )
 
     # > 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.1
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: <=)' )
 
     # パッケージ名が異なる (fail)
     package = Depends::Package.new(<<STATUS)
 Package: d-styles
 Version: 1.0
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: <=)' )
   end
 
   # '<<' な relation のテスト
@@ -126,36 +126,36 @@ STATUS
   def test_relation_lt
     depends = Depends::Dependency.new( 'q-bert (<< 1.0)' )
 
-    assert_equal 'q-bert', depends.name
-    assert_equal '1.0', depends.version
+    assert_equal( 'q-bert', depends.name,    'name 属性が正しくない' )
+    assert_equal( '1.0',    depends.version, 'version 属性が正しくない' )
 
     # == 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.0
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: <<)' )
 
     # < 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 0.9
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定: <<)' )
 
     # > 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.1
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: <<)' )
 
     # パッケージ名が異なる (fail)
     package = Depends::Package.new(<<STATUS)
 Package: d-styles
 Version: 0.9
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: <<)' )
   end
 
   # '=' な relation のテスト
@@ -163,36 +163,36 @@ STATUS
   def test_relation_eql
     depends = Depends::Dependency.new( 'q-bert (= 1.0)' )
 
-    assert_equal 'q-bert', depends.name
-    assert_equal '1.0', depends.version
+    assert_equal( 'q-bert', depends.name,    'name 属性が正しくない' )
+    assert_equal( '1.0',    depends.version, 'version 属性が正しくない' )
 
     # == 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.0
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定: =)' )
 
     # < 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 0.9
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: =)' )
 
     # > 1.0 (fail)
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.1
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: =)' )
 
     # パッケージ名が異なる (fail)
     package = Depends::Package.new(<<STATUS)
 Package: d-styles
 Version: 1.0
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定: =)' )
   end
 
   # バージョンが指定されていない relation のテスト
@@ -200,49 +200,50 @@ STATUS
   def test_relation_noversion
     depends = Depends::Dependency.new( 'q-bert' )
 
-    assert_equal 'q-bert', depends.name
-    assert_equal '*', depends.version
+    assert_equal( 'q-bert', depends.name,    'name 属性が正しくない' )
+    assert_equal( '*',      depends.version, 'version 属性が正しくない' )
 
     # == 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.0
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定無し)' )
 
     # < 1.0
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 0.9
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定無し)' )
 
     # > 1.0 
     package = Depends::Package.new(<<STATUS)
 Package: q-bert
 Version: 1.1
 STATUS
-    assert depends.relation.call( package )
+    assert( depends.relation.call(package), '依存関係が正しくない (バージョン指定無し)' )
 
     # パッケージ名が異なる (fail)
     package = Depends::Package.new(<<STATUS)
 Package: d-styles
 Version: 1.0
 STATUS
-    assert !depends.relation.call( package )
+    assert( !depends.relation.call(package), '依存関係が正しくない (バージョン指定無し)' )
   end
 
   public
   def test_exception
-    assert_raises( Depends::Exception::UnknownDependencyException ) { 
-      Depends::Dependency.new( 'wrong dependency string' )
-    }
+    assert_raises( Depends::Exception::UnknownDependencyException,
+                   'UnknownDependencyException 例外が発生しなかった' ) do
+      Depends::Dependency.new( 'WRONG DEPENDENCY STRING' )
+    end
   end
 
   public
   def test_inspect
     depends = Depends::Dependency.new( 'cpp (>= 4:3.3.2-2)' )
-    assert_equal '<Dependency: cpp (>= 4:3.3.2-2)>', depends.inspect
+    assert_equal( '<Dependency: cpp (>= 4:3.3.2-2)>', depends.inspect, 'inspect 文字列が正しくない' )
   end
 end
 

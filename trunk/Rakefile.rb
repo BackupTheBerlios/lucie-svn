ef6cfea4bfd6e03224df72ad2515bf035dd263b9
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-07-20 07:25:58 +0000

    install-packages 用の rdoc, test ターゲットを追加

--- trunk/Rakefile.rb
+++ trunk/Rakefile.rb
@@ -22,7 +22,7 @@ task :default => [:testall]
 TEST_VERBOSITY = true 
 
 # テストに含まれる全テストスイート
-TEST_SUITES = ['lucie', 'debconf', 'deft', 'lmp', 'depends']
+TEST_SUITES = ['lucie', 'debconf', 'deft', 'lmp', 'depends', 'install-packages']
 
 # すべてのテストは $(TOPDIR)/test/$(テストスイート名)/ というディレクトリを作り
 # tc_*.rb という名前のテストケースに作成する
@@ -58,7 +58,9 @@ Rake::RDocTask.new( :rdoc ) do |rdoc|
   rdoc.options << '--diagram'
   rdoc.rdoc_files.include( 'lib/*.rb', 'lib/debconf/*.rb', 'lib/deft/*.rb', 'lib/depends/*.rb',
                            'lib/lucie/*.rb', 'lib/lucie/config/*.rb', 'lib/lmp/*.rb', 
-                           'data/lmp/default/*.rb', 'data/lmp/compile/*.rb', 'data/lmp/lilo/*.rb', 'data/lmp/network/*.rb',
+                           'data/lmp/default/*.rb', 'data/lmp/compile/*.rb', 
+                           'data/lmp/lilo/*.rb', 'data/lmp/network/*.rb',
+                           'bin/install-packages.rb',
                            'lib/lucie/rake/*.rb' )
 end
 

7cadfb05d1df133c1114bb76e2ef767aedd799c3
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-07-21 07:40:40 +0000

    o chroot していなかったので修正

--- trunk/test/install-packages/tc_install.rb
+++ trunk/test/install-packages/tc_install.rb
@@ -16,7 +16,8 @@ class TC_Install < Test::Unit::TestCase
     install = InstallPackages::Command::Install.new( { 'install' => ['FOO', 'BAR', 'BAZ'] } )
     assert_equal( %{chroot /tmp/target apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" --force-yes --fix-missing install FOO BAR BAZ},
                   install.commandline[0], 'コマンドライン文字列が正しくない' )
-    assert_equal( %{apt-get clean}, install.commandline[1], 'コマンドライン文字列が正しくない' )
+    assert_equal( %{chroot /tmp/target apt-get clean}, 
+                  install.commandline[1], 'コマンドライン文字列が正しくない' )
   end
 end
 

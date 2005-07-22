35a3eb8965aa4b918a141f5ebbcdbc7ee8ea6d14
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-07-22 09:00:16 +0000

    o ファイルを分割

---
+++ trunk/lib/install-packages/command/dselect-upgrade.rb
@@ -0,0 +1,46 @@
+#
+# $Id: install-packages.rb 762 2005-07-22 08:40:17Z takamiya $
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $LastChangedRevision: 762 $
+# License::  GPL2
+
+module InstallPackages
+  module Command
+    class DselectUpgrade < AbstractCommand
+      public
+      def initialize( listHash )
+        super listHash
+        if $dry_run
+          @list['dselect-upgrade'].each do |each|
+            puts( each[:package] + ' ' + each[:action] )
+          end
+        else
+          File.open( tempfile, 'w' ) do |file|
+            @list['dselect-upgrade'].each do |each|
+              file.puts( each[:package] + ' ' + each[:action] )
+            end
+          end
+        end
+      end
+
+      public
+      def commandline
+        return [%{#{root_command} dpkg --set-selections < #{tempfile}},
+          %{#{root_command} apt-get #{APT_OPTION} dselect-upgrade},
+          %{rm #{tempfile}}]
+      end
+      
+      # TODO: use better uniq filename
+      private
+      def tempfile
+        return %{/tmp/target/tmp/dpkg-selections.tmp}
+      end
+    end
+  end
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

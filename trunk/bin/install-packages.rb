4ba58d627ab895d87c586b17098d6b2f96e03d0b
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-07-27 09:19:38 +0000

    o ドキュメントの更新

--- trunk/bin/install-packages.rb
+++ trunk/bin/install-packages.rb
@@ -6,6 +6,8 @@
 # ストールします。/etc/lucie/package ディレクトリ以下のすべての設定ファ
 # イルを読み込みます。文法は非常にシンプルです。
 #
+#   # パッケージ選択設定
+#
 #   taskinst( %w(german) )
 #  
 #   aptitude do |aptitude|
@@ -24,54 +26,98 @@
 # コマンド名は apt-get のコマンドに近いです。以下がサポートしているコ
 # マンドの一覧です。
 #
-# hold: 
-#     パッケージのバージョンを固定します。ホールドされたパッケージは 
-#     dpkg による操作ができなくなります。つまり、アップグレードされな
-#     くなります。
+# <b>hold:</b> 
 #
-# install:
-#     リストで指定したすべてのパッケージをインストールします。パッケー
-#     ジ名の後にハイフンが付けられていた場合 (空白は含まない)、そのパッ
-#     ケージはインストールされず削除されます。
+# パッケージのバージョンを固定します。ホールドされたパッケージは 
+# +dpkg+ によってハンドルされなくなります。つまり、アップグレードされ
+# なくなります。
 #
-# remove:
-#     引数で指定したすべてのパッケージを削除します。パッケージをインス
-#     トールしたい場合には '+' をパッケージ名に続けて記述します。
+# <em>例</em>
+#  hold( %w(ssh less rsync) )
 #
-# taskinst:
-#     引数で指定したタスクに含まれるすべてのパッケージを tasksel(1) を
-#     用いてインストールする。タククのインストールには aptitude を用い
-#     ることもできる。
+# <b>install:</b>
 #
-# aptitude:
-#     すべてのパッケージを aptitude コマンドでインストールする。
+# リストで指定したすべてのパッケージをインストールします。パッケージ名
+# の後にハイフンが付けられていた場合 (間に空白は含まない)、そのパッケー
+# ジはインストールされず削除されます。
 #
-# aptitude-r:
-#     aptitude コマンドと同様だが、aptitude を --with-recommends オプ
-#     ション付きで実行する。
+# <em>例</em>
+#  install do |install|
+#    install.list = %w(ssh tcsh cfengine rsync)
+#  end
 #
-# dselect-upgrade:
-#     パッケージ選択の設定を行います。"dpkg --get-selection" の出力と
-#     同様に、:package にはパッケージ名、:action には実行したいアクショ
-#     ンを指定します。
+# <b>remove:</b>
+#
+# 引数で指定したすべてのパッケージを削除します。パッケージをインストー
+# ルしたい場合には '+' をパッケージ名に続けて記述します。
+#
+# <em>例</em>
+#  remove( %w(rsync jove rstatd) )
+#
+# <b>taskinst:</b>
+#
+# 引数で指定したタスクに含まれるすべてのパッケージを tasksel(1) を用い
+# てインストールします。タスクのインストールには +aptitude+ を用いるこ
+# ともできます。
+#
+# <em>例</em>
+#  taskinst( %w(japanese german) )
+#
+# <b>aptitude:</b>
+#
+# すべてのパッケージを +aptitude+ コマンドでインストールする。
+#
+# <em>例</em>
+#  aptitude do |aptitude|
+#    aptitude.list = %w(ssh tcsh cfengine rsync)
+#  end
+#
+# <b>aptitude-r:</b>
+#
+# aptitude コマンドと同様ですが、+aptitude+ を --with-recommends オ
+# プション付きで実行します
+#
+# <em>例</em>
+#  aptitude do |aptitude|
+#    aptitude.list = %w(ssh tcsh cfengine rsync)
+#  end
+#
+# <b>dselect-upgrade:</b>
+#
+# パッケージ選択の設定を行います。"dpkg --get-selection" の出力と同様
+# に、:package にはパッケージ名、:action には実行したいアクションを指
+# 定します。
+#
+# <em>例</em>
+#   dselect_upgrade do |dselect_upgrade|
+#     dselect_upgrade.list << { :package => 'ddd',  :action => 'install' }
+#     dselect_upgrade.list << { :package => 'a2ps', :action => 'install' }
+#   end
 #
 # すべての依存関係は解決されます。サフィックスに '-' を付けたパッケー
 # ジ名はインストールされる代わりに削除されます。パッケージの順序は意味
 # を持ちません。
 #
-# ブロック中で preloadrm を指定した場合、パッケージのインストール前に 
-# wget(1) を用いたファイルのダウンロードが行われます。file: で指定され
-# る URL を用いることによって、指定されたファイルは Lucie の ROOT ファ
-# イルシステムもしくはインターネット上からダウンロードディレクトリへコ
-# ピーされます。例として、realplayer パッケージはインストールのために
-# アーカイブファイルを必要とするため、アーカイブファイルが /root へダ
-# ウンロードされます。インストールの後このファイルは削除されます。ファ
-# イルを削除したくない場合には、代わりに preload を使用してください。
+# ブロック中で +preloadrm+ を指定した場合、パッケージのインストール前
+# に wget(1) を用いたファイルのダウンロードが行われます。file: で指定
+# される URL を用いることによって、指定されたファイルは Lucie の ROOT 
+# ファイルシステムもしくはインターネット上からダウンロードディレクトリ
+# へコピーされます。例として、realplayer パッケージはインストールのた
+# めにアーカイブファイルを必要とするため、アーカイブファイルが /root 
+# へダウンロードされます。インストールの後このファイルは削除されます。
+# ファイルを削除したくない場合には、代わりに +preload+ を使用してくだ
+# さい。
 #
 #--
 # TODO: utils/chkdebnames、チェックオプションの移植
 # TODO: preload, preloadrm のちゃんとしたサポート
-# TODO: それぞれのコマンドの記述例
+# TODO: hold 引数での文字列サポート
+# TODO: install コマンドでの preloadXX のサポート
+# TODO: ブロック引数を取らない install 形式のサポート
+# TODO: remove 引数での文字列サポート
+# TODO: taskinst 引数での文字列サポート
+# TODO: ブロック引数を取らない aptitude 形式のサポート 
+# TODO: ブロック引数を取らない aptitude-r 形式のサポート 
 #++
 #
 # Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)

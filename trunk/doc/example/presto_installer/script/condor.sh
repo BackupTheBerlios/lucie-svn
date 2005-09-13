f85c73e5de441dc5f0f438ede91b4ec06dc6f4ab
ikuhei <ikuhei@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-09-13 03:49:54 +0000

    change condor.sh

--- trunk/doc/example/presto_installer/script/condor.sh
+++ trunk/doc/example/presto_installer/script/condor.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
+##condorのパッケージをコピーし、インストールするスクリプト##
+##script以下に実行権限付で置くことによりインストール中に実行される##
+
 SOURCE="/etc/lucie/install"
-TARGET="/var/cache/apt/archives"
-LOCAL="/tmp/target"
+LOCALDIR="/var/cache/apt/archives"
 CONDOR_PKG="condor_6.7.10-1_i386.deb"
 
-#cp /etc/lucie/install/condor_6.7.10-1_i386.deb /tmp/target/var/cache/apt/archives/
-cp $SOURCE/$CONDOR_PKG $LOCAL/$TARGET 
-chroot $LOCAL dpkg -i $TARGET/$CONDOR_PKG 
+cp $SOURCE/$CONDOR_PKG $target/$LOCALDIR
+chroot $target dpkg -i $LOCALDIR/$CONDOR_PKG

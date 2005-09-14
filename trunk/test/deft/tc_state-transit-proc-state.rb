ca16d3119c9551839d812c56ca2d0eb61b980d1a
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-09-14 06:33:42 +0000

    o debconf が 30 を返すときのテストを追加

--- trunk/test/deft/tc_state-transit-proc-state.rb
+++ trunk/test/deft/tc_state-transit-proc-state.rb
@@ -14,51 +14,68 @@ require 'mock'
 require 'test/unit'
 
 class TC_StateTransitProcState < Test::Unit::TestCase
+  #
+  # テスト用 debconf 定義として、START state から yes/no によってそれ
+  # ぞれの state に遷移するものを準備。
+  #
+  #
+  #    START (string)
+  #          |
+  #        __|___
+  #       |     |
+  #  'yes'|     |'no'
+  #       |     |
+  #       v     v
+  #      YES    NO
+  #    (note)  (note)
+  #
+  # yes/no 以外は ERROR へ。
+  #
   public
   def setup
-    clear
+    clear_debconf_definition
     
-    template( 'start' ) do |template|
-      template.template_type = 'note'
+    template( 'START' ) do |template|
+      template.template_type = 'string'
       template.short_description_ja = 'スタート'
       template.extended_description_ja = 'スタート'
     end    
-    question( 'start' => proc do |user_input| 
+    question( 'START' => proc do |user_input| 
       case user_input
-      when 'true'
-        'true'
-      when 'false'
-        'false'
+      when 'yes'
+        'YES'
+      when 'no'
+        'NO'
       else
-        'error'
+        'ERROR'
       end
     end ) do |question|
       question.priority = Deft::Question::PRIORITY_MEDIUM
       question.first_question = true
     end
     
-    template( 'true' ) do |template|
+    template( 'YES' ) do |template|
       template.template_type = 'note'
-      template.short_description_ja = 'true'
-      template.extended_description_ja = 'true'
+      template.short_description_ja = 'YES'
+      template.extended_description_ja = 'YES'
     end    
-    question( 'true' ) do |question|
+    question( 'YES' ) do |question|
       question.priority = Deft::Question::PRIORITY_MEDIUM
     end     
-    template( 'false' ) do |template|
+    template( 'NO' ) do |template|
       template.template_type = 'note'
-      template.short_description_ja = 'false'
-      template.extended_description_ja = 'false'
+      template.short_description_ja = 'NO'
+      template.extended_description_ja = 'NO'
     end    
-    question( 'false' ) do |question|
+    question( 'NO' ) do |question|
       question.priority = Deft::Question::PRIORITY_MEDIUM
     end
-    template( 'error' ) do |template|
+    template( 'ERROR' ) do |template|
       template.template_type = 'note'
-      template.short_description_ja = 'error'
-      template.extended_description_ja = 'error'
+      template.short_description_ja = 'ERROR'
+      template.extended_description_ja = 'ERROR'
     end    
-    question( 'error' ) do |question|
+    question( 'ERROR' ) do |question|
       question.priority = Deft::Question::PRIORITY_MEDIUM
     end
     
@@ -67,15 +84,36 @@ class TC_StateTransitProcState < Test::Unit::TestCase
   
   public
   def teardown
-    clear
+    clear_debconf_definition
   end
   
+  # 
+  #   CLIENT        DEBCONF
+  #   
+  #   -------------------->
+  #   'INPUT medium START'
+  #
+  #   <--------------------
+  #            '0'
+  #
+  #   -------------------->
+  #            'GO'
+  #
+  #   <--------------------
+  #            '0'
+  #
+  #   -------------------->
+  #        'GET START'
+  #
+  #   <--------------------
+  #          '0 yes'
+  #    
   public
-  def test_transit_true
+  def test_transit_YES
     $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
     $stdin_mock = Mock.new( '#<STDIN (Mock)>' )
     $stdout_mock.__next( :print ) do |argument| 
-      assert_equal( "INPUT medium start\n", argument ) 
+      assert_equal( "INPUT medium START\n", argument ) 
     end
     $stdin_mock.__next( :gets ) do '0' end    
     $stdout_mock.__next( :print ) do |argument| 
@@ -83,24 +121,45 @@ class TC_StateTransitProcState < Test::Unit::TestCase
     end    
     $stdin_mock.__next( :gets ) do '0' end
     $stdout_mock.__next( :print ) do |argument| 
-      assert_equal( "GET start\n", argument ) 
+      assert_equal( "GET START\n", argument ) 
     end    
-    $stdin_mock.__next( :gets ) do '0 true' end
+    $stdin_mock.__next( :gets ) do '0 yes' end
         
     @debconf_context.transit
-    assert_equal( Deft::ConcreteState['true'], 
+    assert_equal( Deft::ConcreteState['YES'], 
                   @debconf_context.current_state )
     
     $stdout_mock.__verify
     $stdin_mock.__verify
   end 
 
+  # 
+  #   CLIENT        DEBCONF
+  #   
+  #   -------------------->
+  #   'INPUT medium START'
+  #
+  #   <--------------------
+  #            '0'
+  #
+  #   -------------------->
+  #            'GO'
+  #
+  #   <--------------------
+  #            '0'
+  #
+  #   -------------------->
+  #        'GET START'
+  #
+  #   <--------------------
+  #          '0 no'
+  #    
   public
-  def test_transit_false    
+  def test_transit_NO
     $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
     $stdin_mock = Mock.new( '#<STDIN (Mock)>' )
     $stdout_mock.__next( :print ) do |argument| 
-      assert_equal( "INPUT medium start\n", argument ) 
+      assert_equal( "INPUT medium START\n", argument ) 
     end
     $stdin_mock.__next( :gets ) do '0' end    
     $stdout_mock.__next( :print ) do |argument| 
@@ -108,24 +167,45 @@ class TC_StateTransitProcState < Test::Unit::TestCase
     end    
     $stdin_mock.__next( :gets ) do '0' end
     $stdout_mock.__next( :print ) do |argument| 
-      assert_equal( "GET start\n", argument ) 
+      assert_equal( "GET START\n", argument ) 
     end    
-    $stdin_mock.__next( :gets ) do '0 false' end
+    $stdin_mock.__next( :gets ) do '0 no' end
         
     @debconf_context.transit
-    assert_equal( Deft::ConcreteState['false'], 
+    assert_equal( Deft::ConcreteState['NO'], 
                   @debconf_context.current_state )
     
     $stdout_mock.__verify
     $stdin_mock.__verify
   end
   
+  # 
+  #   CLIENT        DEBCONF
+  #   
+  #   -------------------->
+  #   'INPUT medium START'
+  #
+  #   <--------------------
+  #            '0'
+  #
+  #   -------------------->
+  #            'GO'
+  #
+  #   <--------------------
+  #            '0'
+  #
+  #   -------------------->
+  #        'GET START'
+  #
+  #   <--------------------
+  #          '0 hoge'
+  #    
   public
   def test_transit_error    
     $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
     $stdin_mock = Mock.new( '#<STDIN (Mock)>' )
     $stdout_mock.__next( :print ) do |argument| 
-      assert_equal( "INPUT medium start\n", argument ) 
+      assert_equal( "INPUT medium START\n", argument ) 
     end
     $stdin_mock.__next( :gets ) do '0' end    
     $stdout_mock.__next( :print ) do |argument| 
@@ -133,20 +213,54 @@ class TC_StateTransitProcState < Test::Unit::TestCase
     end    
     $stdin_mock.__next( :gets ) do '0' end
     $stdout_mock.__next( :print ) do |argument| 
-      assert_equal( "GET start\n", argument ) 
+      assert_equal( "GET START\n", argument ) 
     end    
     $stdin_mock.__next( :gets ) do '0 hoge' end
         
     @debconf_context.transit
-    assert_equal( Deft::ConcreteState['error'], 
+    assert_equal( Deft::ConcreteState['ERROR'], 
                   @debconf_context.current_state )
     
     $stdout_mock.__verify
     $stdin_mock.__verify
   end  
+
+  # 
+  #   CLIENT        DEBCONF
+  #   
+  #   -------------------->
+  #   'INPUT medium START'
+  #
+  #   <--------------------
+  #            '0'
+  #
+  #   -------------------->
+  #            'GO'
+  #
+  #   <--------------------
+  #            '30'
+  public
+  def test_debconf_returns_30
+    $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
+    $stdin_mock = Mock.new( '#<STDIN (Mock)>' )
+    $stdout_mock.__next( :print ) do |argument| 
+      assert_equal( "INPUT medium START\n", argument ) 
+    end
+    $stdin_mock.__next( :gets ) do '0' end    
+    $stdout_mock.__next( :print ) do |argument| 
+      assert_equal( "GO\n", argument ) 
+    end    
+    $stdin_mock.__next( :gets ) do '30' end
+
+    @debconf_context.transit
+    assert_equal( Deft::ConcreteState['START'], @debconf_context.current_state )
+
+    $stdout_mock.__verify
+    $stdin_mock.__verify
+  end
    
   private
-  def clear
+  def clear_debconf_definition
     Deft::Template.clear
     Deft::Question.clear
     Deft::ConcreteState.clear

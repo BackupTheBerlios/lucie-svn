c1a5bf2130148acb3022bcdf2d9d5a8e2d86a40e
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-09-28 07:42:25 +0000

    追加

---
+++ trunk/data/lmp/condor/deft.rb
@@ -0,0 +1,35 @@
+#
+# $Id: lucie_vm_template.rb 395 2005-03-10 08:28:02Z takamiya $
+#
+# Author::   Yoshiaki Sakae (mailto:sakae@is.titech.ac.jp)
+# Revision:: $LastChangedRevision: 395 $
+# License::  GPL2
+
+require 'deft'
+include Deft
+
+# ------------------------- Welcome メッセージ
+
+template( 'lucie-client/condor/hello' ) do |template|
+  template.template_type = 'note'
+  template.short_description = 'Welcome to lmp-condor setup wizard.'
+  template.extended_description = <<-DESCRIPTION
+  This metapackage will generate Lucie configuration of condor.
+  DESCRIPTION
+  template.short_description_ja = 'lmp-condor セットアップウィザードへようこそ'
+  template.extended_description_ja = <<-DESCRIPTION_JA
+  このメタパッケージは condor の設定を Lucie サーバへ行います。
+  インストーラの sources.list に condor パッケージの apt-line を追加し
+  てください。
+  DESCRIPTION_JA
+end
+
+question( 'lucie-client/condor/hello' ) do |question|
+  question.priority = Question::PRIORITY_MEDIUM
+  question.first_question = true
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

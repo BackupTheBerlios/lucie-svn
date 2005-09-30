2346032aaea867ed9e4451d7aaa3ffadea302fd5
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-09-30 04:21:34 +0000

    * build 時に debian/ 以下のファイルっっを強制上書きするように修正 * リファクタリング

--- trunk/lib/lmp/builder.rb
+++ trunk/lib/lmp/builder.rb
@@ -16,16 +16,15 @@ module LMP
     def initialize( aSpecification, buildDirPathString )
       @spec = aSpecification
       @build_dir = buildDirPathString
-      
-      define_file_task( File.join(@build_dir, 'package') )
-      define_file_task( File.join(@build_dir, 'debian', 'README.Debian'), :readme )
-      define_file_task( File.join(@build_dir, 'debian', 'changelog') )
-      define_file_task( File.join(@build_dir, 'debian', 'config') )
-      define_file_task( File.join(@build_dir, 'debian', 'control') )
-      define_file_task( File.join(@build_dir, 'debian', 'copyright') )
-      define_file_task( File.join(@build_dir, 'debian', 'postinst') )
-      define_file_task( File.join(@build_dir, 'debian', 'rules') )
-      define_file_task( File.join(@build_dir, 'debian', 'templates') )
+
+      task :clean do 
+        debian_files.each do |each|
+          rm each[:path] rescue nil
+        end
+      end
+      debian_files.each do |each|
+        define_file_task( each[:path], each[:template_name] )
+      end
     end
     
     # Specification を元に LMP をビルドする。
@@ -39,15 +38,27 @@ module LMP
     end
        
     private
-    def define_file_task( fileNameString, attribute=nil )
-      file( fileNameString ) do |task|
-        if attribute && @spec.send(attribute)
+    def debian_files
+      [ { :path => File.join(@build_dir, 'debian', 'README.Debian'), :template_name => :readme }, 
+        { :path => File.join(@build_dir, 'debian', 'changelog') },
+        { :path => File.join(@build_dir, 'debian', 'config') },
+        { :path => File.join(@build_dir, 'debian', 'control') },
+        { :path => File.join(@build_dir, 'debian', 'copyright') },
+        { :path => File.join(@build_dir, 'debian', 'postinst') },
+        { :path => File.join(@build_dir, 'debian', 'rules') },
+        { :path => File.join(@build_dir, 'debian', 'templates') } ]
+    end
+
+    private
+    def define_file_task( filePathString, templateNameString=nil )
+      file( filePathString ) do |task|
+        if templateNameString && @spec.send(templateNameString)
           open( task.name, 'w+' ) do |outfile|
-            outfile.print( @spec.send(attribute) )
+            outfile.print( @spec.send(templateNameString) )
           end
-        elsif @spec.send(File.basename(fileNameString))
+        elsif @spec.send(File.basename(filePathString))
           open( task.name, 'w+' ) do |outfile|
-            outfile.print( @spec.send(File.basename(fileNameString)) )
+            outfile.print( @spec.send(File.basename(filePathString)) )
           end
         end
       end     

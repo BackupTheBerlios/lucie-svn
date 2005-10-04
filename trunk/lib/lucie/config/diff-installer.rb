c17923ef83ce62db16457f825f1cc9fa92e6d847
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-10-04 05:52:41 +0000

    rdoc を追加し、コメントを修正。

--- trunk/lib/lucie/config/diff-installer.rb
+++ trunk/lib/lucie/config/diff-installer.rb
@@ -12,8 +12,36 @@ module Lucie
   update(%q$Id$)
 
   module Config
+    #
+    # インストーラ間の差分と共通部分を定義するためのクラス。
+    #
+    # <b>インストーラ:</b> +installer_A+, +installer_B+, +installer_C+, ...
+    # 
+    # があった場合、各インストーラ間の差分 & 共通部分インストーラを以下のように定義できる。
+    #
+    #  Lucie::Config::DiffInstaller.new do |diff_installer|
+    #    diff_installer.name = 'hotswap_node'
+    #    diff_installer.installers = [Installer['installer_A'], Installer['installer_B'], ...]
+    #  end
+    #  
+    #  or 
+    # 
+    #  # 短くしたバージョン
+    #  diff_installer do |diff_installer|
+    #    diff_installer.name = 'hotswap_node'
+    #    diff_installer.installers = [Installer['installer_A'], Installer['installer_B'], ...]
+    #  end
+    #
+    # この場合、共通インストーラとして +hotswap_node+ が明示的に定義さ
+    # れる。また、共通インストーラから各インストーラへの差分インストー
+    # ラが暗に
+    #
+    # * +hotswap_node_to_installer_A+, +hotswap_node_to_installerB+, ...
+    #
+    # という名前で定義される。
+    #
     class DiffInstaller < Resource
-      # 登録されている Host のリスト
+      # 登録されている DiffInstaller のリスト
       @@list = {}
 
       # アトリビュート名のリスト: [:name, :version, ...]

5105f6a43d6296f276eb76da0085d3e2ebb3b7cb
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-10-04 09:45:39 +0000

    o 通常インストール、差分インストールを判断するようにした。

--- trunk/lib/lucie/installer/partition.rb
+++ trunk/lib/lucie/installer/partition.rb
@@ -5,7 +5,14 @@
 # Revision:: $Revision: 557 $
 # License::  GPL2
 
-sh( %{setup_harddisks -f /etc/lucie/partition -d -X > #{$format_log} 2>&1}, $sh_option )
+require 'lucie/nfsroot-task'
+
+if %x{cat #{Rake::NfsrootTask::CONFIGURATION_NAME_STAMP}} == %x{cat #{Rake::NfsrootTask::INSTALLER_NAME_STAMP}}
+  sh( %{setup_harddisks -f /etc/lucie/partition -d -X > #{$format_log} 2>&1}, $sh_option )
+else
+  sh( %{setup_harddisks -f /etc/lucie/partition -d > #{$format_log} 2>&1}, $sh_option )
+end
+
 # TODO: $diskvar が生成されなかった場合のエラー処理
 # TODO: setup_harddisks の再実装
 

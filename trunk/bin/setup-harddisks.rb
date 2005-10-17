5ef2c93776deedc50f6b50bf543ef33a16b34291
sakae <sakae@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-10-17 09:48:12 +0000

    ファイル名変更

---
+++ trunk/bin/setup-harddisks.rb
@@ -0,0 +1,28 @@
+#
+# $Id: lucie-setup.rb 511 2005-04-05 04:59:48Z takamiya $
+#
+# setup-harddisks -- create partitions and filesystems on harddisk
+#
+# This program first read the configfiles, partitions and formats the
+# harddisks, produces fstab.  It uses sfdisk, mke2fs, mkswap
+#
+# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Revision:: $Revision: 511 $
+# License::  GPL2
+#
+# ドキュメントは http://lucie-dev.titech.hpcc.jp:2500/lucie/show/HomePage を
+# 参照のこと
+#
+# Depends: sfdisk, blkid, mke2fs, mkswap, mkfs.reiserfs, mkfs.xfs, swapon, dd, etc.
+
+require 'English'
+require 'lucie/setup-harddisks'
+
+if __FILE__ == $PROGRAM_NAME
+  Lucie::SetupHarddisks.instance.main
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

603dcf971c7266ad0c07e898c523f139c7103e0b
sakae <sakae@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-10-27 05:01:05 +0000

    設定ファイルアップデート

--- trunk/doc/example/presto_installer/partition.rb
+++ trunk/doc/example/presto_installer/partition.rb
@@ -1,5 +1,5 @@
 #
-# setup-hardisks サンプル設定
+# setup-hardisks サンプル設定 for presto_installer
 #
 # $Id: resource.rb 491 2005-03-31 16:10:25Z takamiya $
 #
@@ -12,9 +12,9 @@ require 'lucie/setup-harddisks/config'
 partition "root" do |part|
   part.slice = "/dev/hda"
   part.kind = "primary"
-  part.fs = "ext3"
+  part.fs = "ext2"
   part.mount_point = "/"
-  part.size = (128..256)
+  part.size = (70..200)
   part.bootable = true
   part.fstab_option << "errors=remount-ro"
   part.dump_enabled = true
@@ -22,40 +22,61 @@ end
 
 partition "swap" do |part|
   part.slice = "hda"
-  part.kind = "primary"
+  part.kind = "logical"
   part.fs = "swap"
   part.mount_point = "none"
-  part.size = (256..512)
+  part.size = (40..500)
 end
 
 partition "var" do |part|
   part.slice = "hda"
   part.kind = "logical"
-  part.fs = "reiserfs"
+  part.fs = "ext3"
+  part.format_option << "-m 5"
   part.mount_point = "/var"
-  part.size = 256
+  part.size = (90..1000)
+end
+
+partition "tmp" do |part|
+  part.slice = "hda"
+  part.kind = "logical"
+  part.fs = "ext3"
+  part.format_option << "-m 0"
+  part.mount_point = "/tmp"
+  part.size = (50..1000)
 end
 
 partition "usr" do |part|
   part.slice = "hda"
   part.kind = "logical"
-  part.fs = "reiserfs"
+  part.fs = "ext3"
   part.mount_point = "/usr"
-  part.size = (512..9999999)
+  part.size = (200..4000)
 end
 
 partition "home" do |part|
-  part.slice = "hda7"
+  part.slice = "hda"
   part.kind = "logical"
-  part.fs = "reiserfs"
+  part.fs = "ext3"
+  part.format_option << "-m 1"
   part.mount_point = "/home"
-  part.size = 512
+  part.size = (50..4000)
   part.fstab_option << "nosuid"
 end
 
-#partition "home" do |part|
-#  part.slice = "hda7"
-#  part.mount_point = "/home"
+partition "scratch" do |part|
+  part.slice = "hda10"
+  part.kind = "logical"
+  part.fs = "ext3"
+  part.format_option << "-m 0 -i 50000"
+  part.mount_point = "/scratch"
+  part.size = (50..4000)
+  part.fstab_option << "nosuid"
+end
+
+#partition "scratch" do |part|
+#  part.slice = "hda10"
+#  part.mount_point = "/scratch"
 #  part.preserve = true
 #  part.fstab_option << "nosuid"
 #end

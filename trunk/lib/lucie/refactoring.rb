cb77a668b61113f7d4ac12f6715ac198fb614d4d
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2005-11-19 13:31:43 +0000

    メソッドリネームする際に require する用に追加

---
+++ trunk/lib/lucie/refactoring.rb
@@ -0,0 +1,22 @@
+#
+# $Id: filesystem.rb 958 2005-10-19 09:46:18Z sakae $
+#
+# Author::   Yoshiaki Sakae (mailto:sakae@is.titech.ac.jp)
+# Revision:: $LastChangedRevision: 958 $
+# License::  GPL2
+
+def method_renamed(h)
+  old_name = h.keys[0].to_sym
+  new_name = h.values[0].to_sym
+  define_method(old_name) { |*args|
+    file, line = caller[1].split(':')
+    warning = "##{old_name} renamed to ##{new_name}"
+    $stderr.puts "#{file}:#{line}: #{warning}"
+    send(new_name, *args)
+  }
+end
+
+### Local variables:
+### mode: Ruby
+### indent-tabs-mode: nil
+### End:

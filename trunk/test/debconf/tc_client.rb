09594823145ac552ffbf84575f99fcf9d73c8ad0
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2006-11-27 01:49:39 +0000

    test_parse() のテストを修正

--- trunk/test/debconf/tc_client.rb
+++ trunk/test/debconf/tc_client.rb
@@ -7,24 +7,68 @@
 
 $LOAD_PATH.unshift './lib'
 
+
 require 'mock'
 require 'debconf/client'
 require 'test/unit'
 
+
 class TC_Client < Test::Unit::TestCase
-  public
-  def test_parse_response
-    assert_equal( 'RESPONSE', Debconf::Client.parse_response( '0 RESPONSE' ),
-                  'parse_response の返り値が違う')
-    assert_equal( 30, Debconf::Client.parse_response( '30 RESPONSE' ),
-                  'parse_response の返り値が違う' )
-    assert_equal( 35, Debconf::Client.parse_response( '35 RESPONSE' ),
-                  'parse_response の返り値が違う' )
-    assert_equal( 99, Debconf::Client.parse_response( '99 RESPONSE' ), 
-                  'parse_response の返り値が違う' )
+  def test_parse_response_code_0
+    assert_equal( 'RESPONSE', Debconf::Client.parse_response( '0 RESPONSE' ) )
   end
-  
-  public
+
+
+  def test_parse_response_code_10_19
+    ( 10..19 ).each do | each |
+      assert_raises( Debconf::Exception::InvalidParametersException ) do
+        Debconf::Client.parse_response( "#{ each } ERROR MESSAGE" )
+      end
+    end
+  end
+
+
+  def test_parse_response_code_20_29
+    ( 20..29 ).each do | each |
+      assert_raises( Debconf::Error::SyntaxError ) do
+        Debconf::Client.parse_response( "#{ each } ERROR MESSAGE" )
+      end
+    end
+  end
+
+
+  def test_parse_response_code_30_99
+    ( 30..99 ).each do | each |
+      assert_equal( each, Debconf::Client.parse_response( "#{ each } RESPONSE" ) )
+    end
+  end
+
+
+  def test_parse_response_code_100_109
+    ( 100..109 ).each do | each |
+      assert_raises( Debconf::Error::InternalError ) do
+        Debconf::Client.parse_response( "#{ each } ERROR MESSAGE" )
+      end
+    end
+  end
+
+
+  def test_parse_response_code_unknown
+    ( 110..200 ).each do | each |
+      assert_raises( Debconf::Exception::UnknownReturnValueException ) do
+        Debconf::Client.parse_response( "#{ each } ERROR MESSAGE" )
+      end
+    end
+  end
+
+
+  def test_parse_response_internal_ruby_error
+    assert_raises( Debconf::Error::InternalRubyError ) do
+      Debconf::Client.parse_response( "CANNOT PARSE (CAUSES INTERNAL RUBY ERROR)" )
+    end
+  end
+
+
   def test_go
     $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
     $stdout_mock.__next( :print ) do |command| 
@@ -34,8 +78,8 @@ class TC_Client < Test::Unit::TestCase
     $stdin_mock.__next( :gets ) do '0' end    
     Debconf::Client.go
   end
-  
-  public
+
+
   def test_get
     $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
     $stdout_mock.__next( :print ) do |command| 
@@ -45,8 +89,8 @@ class TC_Client < Test::Unit::TestCase
     $stdin_mock.__next( :gets ) do '0' end    
     Debconf::Client.get( 'QUESTION' )
   end
-  
-  public
+
+
   def test_set
     $stdout_mock = Mock.new( '#<STDOUT (Mock)>' )
     $stdout_mock.__next( :print ) do |command| 
@@ -57,6 +101,7 @@ class TC_Client < Test::Unit::TestCase
     Debconf::Client.set( 'QUESTION true' )
   end
 end
+
 
 ### Local variables:
 ### mode: Ruby

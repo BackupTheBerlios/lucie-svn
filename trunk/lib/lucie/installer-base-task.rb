8a767de7a46e10c84440a8c34bf411483fc34448
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2006-12-13 04:45:13 +0000

    リファクタリング

--- trunk/lib/lucie/installer-base-task.rb
+++ trunk/lib/lucie/installer-base-task.rb
@@ -1,166 +1,112 @@
 #
 # $Id$
 #
-# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
 # Revision:: $LastChangedRevision$
-# License::  GPL2
+# License:: GPL2
 
+
+require 'lucie/apt'
+require 'lucie/chroot'
 require 'lucie/command-line-options'
-require 'lucie/time-stamp'
+require 'lucie/debootstrap'
+require 'lucie/shell'
 require 'rake'
 require 'rake/tasklib'
 
-Lucie::update(%q$Id$)
 
 module Rake
   class InstallerBaseTask < TaskLib
-    # base インストーラが作成されるディレクトリ
     INSTALLER_BASE_DIR = '/var/lib/lucie/installer_base'.freeze
 
-    # base インストーラの名前
-    attr_accessor :name
-    # base インストーラの作成に使用される一時ディレクトリのフルパス
-    attr_accessor :dir
-    # debootstrap の実行で使用される Debian ミラーサーバの URI
-    attr_accessor :mirror
-    # base インストーラを作成するディストリビューション
-    attr_accessor :distribution
-    # base インストーラを作成するディストリビューションのバージョン
-    attr_accessor :distribution_version
 
-    # 作成される tgz ファイルのファイル名を返す
-    def self.target_fname( distribution, distribution_version )
-      return distribution + '_' + distribution_version + '.tgz'
+    attr_accessor :name
+    attr_accessor :target_directory
+    attr_accessor :mirror
+    attr_accessor :distribution
+    attr_accessor :suite
+
+
+    def self.target_fname( distribution, suite )
+      return distribution + '_' + suite + '.tgz'
     end
-    
-    #
-    # 以下のようにすることでインストーラ "my_installer" のベースインス
-    # トーラを作成する Task が定義される。
-    #
-    #  installer_name = 'my_installer'
-    #  Rake::InstallerBaseTask.new( installer_name ) do |installer_base|
-    #    installer_base.dir = File.join( Rake::InstallerBaseTask::INSTALLER_BASE_DIR, installer_name )
-    #    installer_base.mirror = 'http://192.168.152.2:9999/debian'
-    #    installer_base.distribution = 'debian'
-    #    installer_base.distribution_version = 'sarge'
-    #  end
-    #
-    # 定義される Task は以下の通り (インストーラ名を "my_installer" とした場合)。
-    # 
-    # * _my_installer_: base インストーラの作成
-    # * _clobber_my_installer_: base インストーラの削除
-    # * _remy_installer_: base インストーラの強制再ビルド
-    # 
-    public
+
+
     def initialize( name=:installer_base ) # :yield: self
       @name = name
-      @dir = INSTALLER_BASE_DIR
+      @target_directory = INSTALLER_BASE_DIR
       @mirror = 'http://www.debian.or.jp/debian/'
       yield self if block_given?
       define
     end
-    
+
+
     private
+
+
     def define
-      desc "Build the #{distribution} version #{distribution_version} installer base tarball"
-      task name
-      
-      desc "Force a rebuild of the installer base tarball"
-      task paste("re", name) => [paste("clobber_", name), name]
-      
-      desc "Remove installer base filesystem"
-      task paste("clobber_", name) do 
+      desc "Build installer base tarball for #{ @distribution } distribution, version = ``#{ @suite }''."
+      task @name
+
+      desc "Force a rebuild of the installer base tarball."
+      task paste( "re", @name ) => [ paste( "clobber_", @name ), @name ]
+
+      desc "Remove the installer base tarball."
+      task paste( "clobber_", @name ) do
         cleanup_temporary_directory
       end
-      
-      task :clobber => [paste("clobber_", name)]
-      
-      directory @dir
-      task name => [installer_base_target]
-      
+
+      task :clobber => [ paste( "clobber_", @name ) ]
+
+      directory @target_directory
+      task @name => [ installer_base_target ]
+
       file installer_base_target do
-        debootstrap_option = "--arch i386 --exclude=#{exclude_packages.join(',')} --include=ncurses-term,locales"
-        info "Executing debootstrap. This may take a long time."
-        sh_log( %{yes '' | LC_ALL=C debootstrap #{debootstrap_option} #{@distribution_version} #{@dir} #{@mirror} 2>&1}, sh_option ) do |rd|
-          line_length = 0
-          while (rd.gets)
-            line = $_.chomp
-            case line
-            when /^I: /
-              STDERR.print ' ' * line_length, "\r"
-              STDERR.print line, "\r"
-              line_length = line.length
-              logger.info line
-            when /^E: /
-              logger.error line 
-              raise DebootstrapExecutionError, line
-            else
-              logger.debug line 
-            end
-          end
+        info "Creating base system using debootstrap version #{ Debootstrap.VERSION }"
+        info "Calling debootstrap #{ suite } #{ target_directory } #{ mirror }"
+        Debootstrap.new do | option |
+          option.env = { 'LC_ALL' => 'C' }
+          option.exclude = [ 'dhcp-client', 'info' ]
+          option.suite = @suite
+          option.target = @target_directory
+          option.mirror = @mirror
         end
-        sh %{chroot #{@dir} apt-get clean}, sh_option
-        rm File.join(@dir, '/etc/resolv.conf'), { :force => true }.merge( sh_option )
-        locale_gen
+
+        Apt.new( :clean ) do | option |
+          option.root = @target_directory
+        end
+
+        Kernel.sh 'rm', File.join( @target_directory, '/etc/resolv.conf' )
         build_installer_base_tarball
         cleanup_temporary_directory
       end
     end
 
-    private
+
     def build_installer_base_tarball
       info "Creating installer base tarball on #{installer_base_target}."
-      sh %{tar -l -C #{@dir} -cf - . | gzip > #{installer_base_target}}, sh_option
+      Kernel.sh 'tar', '-l', '--directory', @target_directory, '-czvf', installer_base_target, '.'
     end
 
-    private 
+
     def cleanup_temporary_directory
       info "Removing debootstrap temporary directory."
-      rm_rf @dir, sh_option
-    end
-    
-    # メタパッケージ用に locale を作成
-    private
-    def locale_gen
-      File.open( File.join(@dir, '/etc/locale.gen'), 'w+' ) do |file|
-        file.puts "ja_JP.EUC-JP EUC-JP"
-        file.puts "en_US ISO-8859-1"
-      end
-      sh %{chroot #{@dir} locale-gen}, sh_option
+      Kernel.sh 'rm', '-rf', @target_directory
     end
 
-    private
+
     def info( aString )
-      logger.info aString
+      # logger.info aString
       puts aString
     end
 
-    private
-    def logger
-      return Lucie::Logger::instance
-    end 
 
-    private
-    def sh_option
-       return {:verbose => Lucie::CommandLineOptions.instance.verbose} 
-    end
-    
-    private
-    def exclude_packages
-      return ['pcmcia-cs,ppp', 'pppconfig', 'pppoe', 'pppoeconf', 
-               'dhcp-client', 'exim4', 'exim4-base', 'exim4-config',
-               'exim4-daemon-light', 'mailx', 'at', 'fdutils', 'info', 
-               'modconf', 'libident', 'logrotate', 'exim'] 
-    end
-    
-    private
     def installer_base_target
-      return File.join(INSTALLER_BASE_DIR, InstallerBaseTask.target_fname( @distribution, @distribution_version ))
+      return File.join( @target_directory, InstallerBaseTask.target_fname( @distribution, @suite ) )
     end
-    
-    class DebootstrapExecutionError < ::StandardError; end
   end
 end
+
 
 ### Local variables:
 ### mode: Ruby
--- trunk/test/lucie/tc_installer-base-task.rb
+++ trunk/test/lucie/tc_installer-base-task.rb
@@ -1,104 +1,170 @@
 #
 # $Id$
 #
-# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
 # Revision:: $LastChangedRevision$
-# License::  GPL2
+# License:: GPL2
+
 
 $LOAD_PATH.unshift './lib'
 
+
+require 'flexmock'
 require 'rake'
 require 'lucie/installer-base-task'
 require 'test/unit'
 
+
 class TC_InstallerBaseTask < Test::Unit::TestCase
-  public
+  include FlexMock::TestCase
+
+
   def setup
-    Task.clear    
-  end
-  
-  public
-  def teardown 
     Task.clear
   end
-  
-  public
-  def test_installer_base_target_prerequisites
-    Rake::InstallerBaseTask.new do |task|
-      task.distribution = 'debian'
-      task.distribution_version = 'woody'
-    end
-    assert_equal( ['/var/lib/lucie/installer_base/debian_woody.tgz'], 
-                  Task[:installer_base].prerequisites,
-                  ":installer_base タスクの prerequisites が正しくない" )
-  end
-  
-  public
-  def test_clobber_target_prerequisites
-    Rake::InstallerBaseTask.new do |task|
-      task.distribution = 'debian'
-      task.distribution_version = 'woody'
-    end
-    assert_equal( ["clobber_installer_base"], 
-                  Task[:clobber].prerequisites,
-                  ":clobber タスクの prerequisites が正しくない" )
-  end
-  
-  public
-  def test_reinstaller_base_target_prerequisites
-    Rake::InstallerBaseTask.new do |task|
-      task.distribution = 'debian'
-      task.distribution_version = 'woody'
-    end
-    assert_equal( ["clobber_installer_base", "installer_base"], 
-                  Task[:reinstaller_base].prerequisites,
-                  ":reinstaller_base タスクの prerequisites が正しくない" )
-  end
-  
-  public
-  def test_targets_defined
-    Rake::InstallerBaseTask.new do |task|
-      task.distribution = 'debian'
-      task.distribution_version = 'woody'
-    end
-    assert_not_nil( Task[:installer_base], 
-                    ':installer_base タスクが定義されていない' )
-    assert_equal( "Build the debian version woody installer base tarball",
-                  Task[:installer_base].comment, 
-                  ":installer_base タスクのコメントが設定されていない" )
-    
-    assert_not_nil( Task[:reinstaller_base],
-                    ':reinstaller_base タスクが定義されていない' )
-    assert_equal( "Force a rebuild of the installer base tarball",
-                  Task[:reinstaller_base].comment, 
-                  ":reinstaller_base タスクのコメントが設定されていない" )
-    
-    assert_not_nil( Task[:clobber_installer_base],
-                    ':clobber_installer_base タスクが定義されていない' )
-    assert_equal( "Remove installer base filesystem",
-                  Task[:clobber_installer_base].comment, 
-                  ":clobber_installer_base タスクのコメントが設定されていない" )
-    
-    assert_not_nil( Task['/var/lib/lucie/installer_base'],
-                    '/var/lib/lucie/installer_base ディレクトリタスクが定義されていない' )
 
-    assert_not_nil( Task['/var/lib/lucie/installer_base/debian_woody.tgz'],
-                    '/var/lib/lucie/installer_base/var/tmp/debian_woody.tgz ファイルタスクが定義されていない' )
+
+  def teardown
+    Task.clear
   end
-  
-  public
+
+
   def test_accessor
-    installer_base_task = Rake::InstallerBaseTask.new do |task|
-      task.dir = 'tmp'
-      task.distribution = 'debian'
-      task.distribution_version = 'sarge'
+    installer_base_task = Rake::InstallerBaseTask.new do | task |
+      task.target_directory = '/TMP'
+      task.mirror = 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/'
+      task.distribution = 'DEBIAN'
+      task.suite = 'SARGE'
     end
-    assert_equal( :installer_base, installer_base_task.name )
-    assert_equal( 'tmp', installer_base_task.dir )
-    assert_equal( 'debian', installer_base_task.distribution )
-    assert_equal( 'sarge', installer_base_task.distribution_version )
+    assert_equal :installer_base, installer_base_task.name
+    assert_equal '/TMP', installer_base_task.target_directory
+    assert_equal 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/', installer_base_task.mirror
+    assert_equal 'DEBIAN', installer_base_task.distribution
+    assert_equal 'SARGE', installer_base_task.suite
+  end
+
+
+  def test_all_targets_defined
+    Rake::InstallerBaseTask.new do | task |
+      task.target_directory = '/TMP'
+      task.mirror = 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/'
+      task.distribution = 'DEBIAN'
+      task.suite = 'SARGE'
+    end
+
+    assert_kind_of Rake::Task, Task[ :installer_base ]
+    assert_kind_of Rake::Task, Task[ :reinstaller_base ]
+    assert_kind_of Rake::Task, Task[ :clobber_installer_base ]
+    assert_kind_of Rake::Task, Task[ '/TMP' ]
+    assert_kind_of Rake::Task, Task[ '/TMP/DEBIAN_SARGE.tgz' ]
+
+    assert_equal( "Build installer base tarball for DEBIAN distribution, version = ``SARGE''.", Task[ :installer_base ].comment )
+    assert_equal( "Force a rebuild of the installer base tarball.", Task[ :reinstaller_base ].comment )
+    assert_equal( "Remove the installer base tarball.", Task[ :clobber_installer_base ].comment )
+  end
+
+
+  def test_installer_base_target_prerequisites
+    Rake::InstallerBaseTask.new do | task |
+      task.target_directory = '/TMP/'
+      task.distribution = 'DEBIAN'
+      task.suite = 'WOODY'
+    end
+    assert_equal [ '/TMP/DEBIAN_WOODY.tgz' ], Task[ :installer_base ].prerequisites
+  end
+
+
+  def test_clobber_target_prerequisites
+    Rake::InstallerBaseTask.new do | task |
+      task.distribution = 'DEBIAN'
+      task.suite = 'WOODY'
+    end
+    assert_equal [ "clobber_installer_base" ], Task[ :clobber ].prerequisites
+  end
+
+
+  def test_clobber_installer_base_target
+    flexstub( Shell, 'SHELL_CLASS' ).should_receive( :open ).with( Proc ).once.ordered.and_return do | block |
+      shell = flexmock( 'SHELL' )
+      shell.should_receive( :exec ).with( { 'LC_ALL' => 'C' }, 'rm', '-rf', '/TMP' ).once.ordered
+      block.call shell
+    end
+
+    Rake::InstallerBaseTask.new do | task |
+      task.target_directory = '/TMP'
+      task.mirror = 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/'
+      task.distribution = 'DEBIAN'
+      task.suite = 'SARGE'
+    end
+    Task[ :clobber_installer_base ].execute
+  end
+
+
+  def test_reinstaller_base_target_prerequisites
+    Rake::InstallerBaseTask.new do | task |
+      task.distribution = 'DEBIAN'
+      task.suite = 'WOODY'
+    end
+    assert_equal [ "clobber_installer_base", "installer_base" ], Task[ :reinstaller_base ].prerequisites
+  end
+
+
+  def test_installer_base_target
+    flexstub( Shell, 'SHELL_CLASS' ).should_receive( :open ).with( Proc ).once.ordered.and_return do | block |
+      flexmock( 'SHELL' ) do | shell |
+        shell.should_receive( :on_stdout ).with( Proc ).once.ordered.and_return do | stdout_block |
+          stdout_block.call 'ii  debootstrap    0.2.45-0.2     Bootstrap a basic Debian system'
+        end
+        shell.should_receive( :exec ).with( { 'LC_ALL' => 'C' }, 'dpkg', '-l' ).once.ordered
+        block.call shell
+      end
+    end
+    flexstub( Debootstrap, 'DEBOOTSTRAP' ).should_receive( :new ).with( Proc ).once.ordered.and_return do | block |
+      debootstrap_option_mock = flexmock( 'DEBOOTSTRAP_OPTION' )
+      debootstrap_option_mock.should_receive( :env= ).with( { 'LC_ALL' => 'C' } ).once.ordered
+      debootstrap_option_mock.should_receive( :exclude= ).with( [ 'dhcp-client', 'info' ] ).once.ordered
+      debootstrap_option_mock.should_receive( :suite= ).with( 'SARGE' ).once.ordered
+      debootstrap_option_mock.should_receive( :target= ).with( '/TMP' ).once.ordered
+      debootstrap_option_mock.should_receive( :mirror= ).with( 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/' ).once.ordered
+      block.call debootstrap_option_mock
+    end
+
+    flexstub( Apt, 'APT' ).should_receive( :new ).with( :clean, Proc ).once.ordered.and_return do | command, block |
+      apt_option_mock = flexmock( 'APT_OPTION' )
+      apt_option_mock.should_receive( :root= ).with( '/TMP' ).once.ordered
+      block.call apt_option_mock
+    end
+
+    flexstub( Shell, 'SHELL_CLASS' ).should_receive( :open ).with( Proc ).once.ordered.and_return do | block |
+      shell = flexmock( 'SHELL' )
+      shell.should_receive( :exec ).with( { 'LC_ALL' => 'C' }, 'rm', '/TMP/etc/resolv.conf' ).once.ordered
+      block.call shell
+    end
+
+    # mocking build_installer_base_tarball
+    flexstub( Shell, 'SHELL_CLASS' ).should_receive( :open ).with( Proc ).once.ordered.and_return do | block |
+      shell = flexmock( 'SHELL' )
+      shell.should_receive( :exec ).with( { 'LC_ALL' => 'C' }, 'tar', '-l', '--directory', '/TMP', '-czvf', '/TMP/DEBIAN_SARGE.tgz', '.' ).once.ordered
+      block.call shell
+    end
+
+    # mocking cleanup_temporary_directory
+    flexstub( Shell, 'SHELL_CLASS' ).should_receive( :open ).with( Proc ).once.ordered.and_return do | block |
+      shell = flexmock( 'SHELL' )
+      shell.should_receive( :exec ).with( { 'LC_ALL' => 'C' }, 'rm', '-rf', '/TMP' ).once.ordered
+      block.call shell
+    end
+
+    Rake::InstallerBaseTask.new do | task |
+      task.target_directory = '/TMP'
+      task.mirror = 'HTTP://WWW.DEBIAN.OR.JP/DEBIAN/'
+      task.distribution = 'DEBIAN'
+      task.suite = 'SARGE'
+    end
+    Task[ '/TMP/DEBIAN_SARGE.tgz' ].execute
   end
 end
+
 
 ### Local variables:
 ### mode: Ruby

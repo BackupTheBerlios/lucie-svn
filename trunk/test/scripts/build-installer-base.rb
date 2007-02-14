e89d180e660686c0db17b907f242c547bf36e3cc
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2007-02-14 04:18:06 +0000

    added.

---
+++ trunk/test/scripts/build-installer-base.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+#
+# $Id$
+#
+# Author:: Yasuhito Takamiya (mailto:yasuhito@gmail.com)
+# Revision:: $LastChangedRevision$
+# License:: GPL2
+
+
+$LOAD_PATH.unshift( '../../lib' ) if __FILE__ =~ /\.rb$/
+
+
+require 'lucie/installer-base-task'
+
+
+Rake::InstallerBaseTask.new do | task |
+  task.target_directory = '/tmp/debootstrap'
+  task.mirror = 'http://ring.asahi-net.or.jp/archives/linux/debian/debian/'
+  task.distribution = 'debian'
+  task.suite = 'sarge'
+  task.http_proxy = 'http://proxy.spf.cl.nec.co.jp:3128'
+end
+
+
+Task[ 'reinstaller_base' ].invoke

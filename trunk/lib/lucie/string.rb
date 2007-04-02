dd945bdf2ef7ee2b5bace39d66ffc5fad93b3d56
takamiya <takamiya@0683cf09-06ee-0310-9ef3-b6387079bcae>
2007-04-02 07:35:46 +0000

    o リファクタリング o カバレッジが 100% になるように修正

--- trunk/lib/lucie/string.rb
+++ trunk/lib/lucie/string.rb
@@ -1,98 +1,93 @@
 #
 # $Id$
 #
-# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Author::   Yasuhito Takamiya (mailto:yasuhito@gmail.com)
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
-require 'lucie/time-stamp'
-
-Lucie::update(%q$Id$)
 
 # 標準の String クラスにインデント系のメソッドを追加
 class String
-  public
-  def indent( n )
-    return map do |each|
+  def indent n
+    return map do | each |
       ' ' * n + each
-    end
+    end.join
   end
-  
+
+
   # インデントを保ちつつ左寄せする
-  public
   def unindent_auto
     return unindent( minimum_indent )
   end
-  
+
+
   # 最小のインデント幅を返す。タブストップは 8 と計算する。
-  public
   def minimum_indent
-    indents = map do |each|
-      each.rstrip.untabify.slice(/\A */).length
+    indents = map do | each |
+      each.rstrip.untabify.slice( /\A */ ).length
     end
-    return ((indents - [0]).min || 0)
+    return ( ( indents - [ 0 ] ).min || 0 )
   end
-  
+
+
   # 文字列を n ぶんアンインデントする
-  public
-  def unindent( n, tabstopNum = 8 )
+  def unindent n, tabstopNum = 8
     indent_re = /^ {0,#{n}}/
-    return map do |each|
-      if tabstopNum
-        each.untabify( tabstopNum ).sub( indent_re, '' ).tabify tabstopNum
-      else
-        each.sub indent_re, ''
-      end
-    end.join('')
-  end   
-  
+    return map do | each |
+      each.untabify( tabstopNum ).sub( indent_re, '' ).tabify tabstopNum
+    end.join( '' )
+  end
+
+
   # タブをスペースに変換する
   #--
   # NOTE: don't work with UTF-8
   #++
-  public
-  def untabify( tabstopNum = 8 )
-    return gsub(/(.*?)\t/n) do $1 + ' ' * (tabstopNum - ($1.length % tabstopNum)) end
+  def untabify tabstopNum = 8
+    return gsub( /(.*?)\t/n ) do
+      $1 + ' ' * ( tabstopNum - ( $1.length % tabstopNum ) )
+    end
   end
-  
+
+
   # 行頭のスペースをタブとスペースに変換する
   #--
   # NOTE: don't work with UTF-8
   #++
-  public
-  def tabify( tabstopNum = 8)
-    return gsub(/^[ \t]+/) do |each|
+  def tabify tabstopNum = 8
+    return gsub( /^[ \t]+/ ) do | each |
       ntabs, nspaces = each.untabify( tabstopNum ).length.divmod( tabstopNum )
       "\t" * ntabs + ' ' * nspaces
     end
   end
-  
+
+
   # 'pascal_style' => 'PascalStyle' のように変換
-  public
   def to_pascal_style
-    if self.include?('_')
-      return self.split('_').collect do |each| 
-        each.capitalize 
+    if self.include?( '_' )
+      return self.split( '_' ).collect do | each |
+        each.capitalize
       end.join
     else
       return self.capitalize
     end
-  end  
-  
+  end
+
+
   # 長いリテラルを RFC822 に合うようにフォーマットする。
-  # Debconf のテンプレートの extended description に用いる。    
-  public
+  # Debconf のテンプレートの extended description に用いる。
   def to_rfc822
-    return unindent_auto.split("\n").map do |each|
+    return unindent_auto.map do | each |
       case each
       when /\A\s*\Z/
-          ' .'
+        " .\n"
       else
-          " #{each}"
+        " #{each}"
       end
-    end.join("\n")
+    end.join( "" )
   end
 end
+
 
 ### Local variables:
 ### mode: Ruby
--- trunk/test/tc_string.rb
+++ trunk/test/tc_string.rb
@@ -1,166 +1,219 @@
 #
 # $Id$
 #
-# Author::   Yasuhito Takamiya (mailto:takamiya@matsulab.is.titech.ac.jp)
+# Author::   Yasuhito Takamiya (mailto:yasuhito@gmail.com)
 # Revision:: $LastChangedRevision$
 # License::  GPL2
 
+
 $LOAD_PATH.unshift './lib'
+
 
 require 'lucie/string'
 require 'test/unit'
 
+
 class TC_String < Test::Unit::TestCase
+  def test_to_rfc822
+    assert_equal( ( <<-EXPECTED_STRING ), ( <<-TEST_STRING ).to_rfc822, 'String#to_rfc822 が正しい値を返さない' )
+ foo
+ .
+ bar
+ .
+ baz
+    EXPECTED_STRING
+foo
+
+bar
+
+baz
+    TEST_STRING
+  end
+
+
+  def test_to_pascal_style
+    assert_equal 'Foo', 'foo'.to_pascal_style, 'String#to_pascal_style が正しい値を返さない'
+  end
+
+
+  def test_to_pascal_style_with_underbar
+    assert_equal 'FooBarBaz', 'foo_bar_baz'.to_pascal_style, 'String#to_pascal_style が正しい値を返さない'
+  end
+
+
   # ソースコードが左寄せになることを確認
-  public
   def test_unindent_auto
-    assert_equal( (<<-EXPECTED_SOURCE_CODE), (<<-TEST_SOURCE_CODE).unindent_auto, "正しくアンインデント(オート)できない" )
+    assert_equal( ( <<-EXPECTED_STRING ), ( <<-TEST_STRING ).unindent_auto, "正しく unindent_auto できない" )
 class Test
   public
   def this_is_da_test
     puts "HELLO"
   end
 end
-    EXPECTED_SOURCE_CODE
+    EXPECTED_STRING
     class Test
       public
       def this_is_da_test
         puts "HELLO"
       end
     end
-    TEST_SOURCE_CODE
+    TEST_STRING
   end
-  
+
+
+  def test_indent
+    assert_equal ' 0123', '0123'.indent( 1 )
+  end
+
+
+  def test_indent_multiple_lines
+    assert_equal( ( <<-EXPECTED_STRING ), ( <<-TEST_STRING ).indent( 4 ), "正しく indent できない" )
+    class Test
+      public
+      def this_is_da_test
+        puts "HELLO"
+      end
+    end
+    EXPECTED_STRING
+class Test
+  public
+  def this_is_da_test
+    puts "HELLO"
+  end
+end
+    TEST_STRING
+  end
+
+
   # スペース -> タブのテスト
-  public
   def test_tabify
-    assert_equal "\t ABCDE", '         ABCDE'.tabify, '正しくタビファイできない'
+    assert_equal "\t ABCDE", '         ABCDE'.tabify, '正しく tabify できない'
   end
-  
+
+
   # タブ -> スペースのテスト
-  public
   def test_untabify_tabstop_default
-    assert_equal '        ABCDE', "\tABCDE".untabify, '正しくアンタビファイできない'
+    assert_equal '        ABCDE', "\tABCDE".untabify, '正しく untabify できない'
   end
-  
+
+
   # タブ -> スペースのテスト (タブストップを変えて)
-  public
   def test_untabify_tabstop_4
-    assert_equal '    ABCDE', "\tABCDE".untabify( 4 ), '正しくアンタビファイできない'
+    assert_equal '    ABCDE', "\tABCDE".untabify( 4 ), '正しく untabify できない'
   end
-  
+
+
   # タブ -> スペースのテスト (凝った例)
-  public
   def test_untabify_complicated_string
-    assert_equal 'ABCDE   FGHIJK  LMN', "ABCDE\tFGHIJK\tLMN".untabify( 4 ), '正しくアンタビファイできない'
+    assert_equal 'ABCDE   FGHIJK  LMN', "ABCDE\tFGHIJK\tLMN".untabify( 4 ), '正しく untabify できない'
   end
-  
+
+
   # アンインデントしても変わらず
-  public
   def test_unindent_0
-    assert_equal 'ABCDE', 'ABCDE'.unindent(0), "正しくアンインデントされていない"
+    assert_equal 'ABCDE', 'ABCDE'.unindent( 0 ), "正しく unindent されていない"
   end
-  
-  # アンインデントしても変わらず (複数行)
-  public
+
+
+  # アンインデントしても変わらず
   def test_unindent_0_multiple_lines
-    assert_equal (<<-EXPECTED_STRING_UNINDENT_0), (<<-TEST_STRING_UNINDENT_0).unindent(0), "正しくアンインデントされていない"
+    assert_equal( ( <<-EXPECTED_STRING ), ( <<-TEST_STRING ).unindent( 0 ), "正しく unindent されていない" )
  ABCDE
  FGHIJ
  KLMNO
-    EXPECTED_STRING_UNINDENT_0
+    EXPECTED_STRING
  ABCDE
  FGHIJ
  KLMNO
-    TEST_STRING_UNINDENT_0
+    TEST_STRING
   end
-  
+
+
   # 4 つアンインデント
-  public
   def test_unindent_4
-    assert_equal ' ABCDE', '     ABCDE'.unindent(4), "正しくアンインデントされていない"
+    assert_equal ' ABCDE', '     ABCDE'.unindent( 4 ), "正しく unindent されていない"
   end
-  
-  # 4 つアンインデント (複数行)
-  public
+
+
+  # 4 つアンインデント
   def test_unindent_4_multiple_lines
-    assert_equal (<<-EXPECTED_STRING_UNINDENT_4), (<<-TEST_STRING_UNINDENT_4).unindent(4), "正しくアンインデントされていない"
+    assert_equal( ( <<-EXPECTED_STRING ), ( <<-TEST_STRING ).unindent( 4 ), "正しくアンインデントされていない" )
   ABCDE
 FGHIJ
-  KLMNO    
-    EXPECTED_STRING_UNINDENT_4
+  KLMNO
+    EXPECTED_STRING
       ABCDE
     FGHIJ
-      KLMNO    
-    TEST_STRING_UNINDENT_4
+      KLMNO
+    TEST_STRING
   end
-  
-  # タブを含めて 3 つアンインデント 
-  public
+
+
+  # タブを含めて 3 つアンインデント
   def test_unindent_with_tab
-    assert_equal "      ABCDE", "\t ABCDE".unindent(3), "正しくアンインデントされていない"
+    assert_equal "      ABCDE", "\t ABCDE".unindent( 3 ), "正しく unindent されていない"
   end
-  
-  # タブを含めて 3 つアンインデント (複数行) 
-  public
+
+
+  # タブを含めて 3 つアンインデント
   def test_unindent_with_tab_multiple_lines
-    assert_equal (<<-EXPECTED_STRING_UNINDENT_WITH_TAB), (<<-TEST_STRING_UNINDENT_WITH_TAB).unindent(3), "正しくアンインデントされていない"
+    assert_equal( ( <<-EXPECTED_STRING ), ( <<-TEST_STRING ).unindent( 3 ), "正しく unindent されていない" )
        ABCDE
      FGHIJ
-       KLMNO    
-    EXPECTED_STRING_UNINDENT_WITH_TAB
+       KLMNO
+    EXPECTED_STRING
 \t  ABCDE
 \tFGHIJ
-\t  KLMNO    
-    TEST_STRING_UNINDENT_WITH_TAB
+\t  KLMNO
+    TEST_STRING
   end
-  
+
+
   # 頭に空白がないため、minimum_indent は 0
-  public
   def test_minimum_indent_0
-    assert_equal 0, 'ABCDE'.minimum_indent, "minimum_indent の値がおかしい"
+    assert_equal 0, 'ABCDE'.minimum_indent, "minimum_indent の返す値が正しくない"
   end
-  
-  # 頭に空白がないため、minimum_indent は 0 (複数行)
-  public
+
+
+  # 頭に空白がないため、minimum_indent は 0
   def test_minimum_indent_0_multiple_lines
-    assert_equal 0, (<<-TEST_STRING_MINIMUM_INDENT_0).minimum_indent, "minimum_indent の値がおかしい"
+    assert_equal 0, ( <<-TEST_STRING ).minimum_indent, "minimum_indent の返す値が正しくない"
 ABCDE
 FGHIJ
 KLMNO
-    TEST_STRING_MINIMUM_INDENT_0
+    TEST_STRING
   end
-  
+
+
   # 頭に空白が 4 つあるので、minimum_indent は 4
-  public
   def test_minimum_indent_4
-    assert_equal 4, '    ABCDE'.minimum_indent, "minimum_indent の値がおかしい"
+    assert_equal 4, '    ABCDE'.minimum_indent, "minimum_indent の返す値が正しくない"
   end
-  
-  # 頭に空白が 4 つあるので、minimum_indent は 4 (複数行)
-  public
+
+
+  # 頭に空白が 4 つあるので、minimum_indent は 4
   def test_minimum_indent_4_multiple_lines
-    assert_equal 4, (<<-TEST_STRING_MINIMUM_INDENT_4).minimum_indent, "minimum_indent の値がおかしい"
+    assert_equal 4, ( <<-TEST_STRING ).minimum_indent, "minimum_indent の返す値が正しくない"
       ABCDE
     FGHIJ
       KLMNO    
-    TEST_STRING_MINIMUM_INDENT_4
+    TEST_STRING
   end
-  
-  # タブストップ 8 なので、頭にタブがあった場合 minimum_indent は 8
-  public
+
+
+  # タブストップ 8 なので、minimum_indent は 8
   def test_minimum_indent_8
-    assert_equal 8, "\tABCDE".minimum_indent, "minimum_indent の値がおかしい"
+    assert_equal 8, "\tABCDE".minimum_indent, "minimum_indent の返す値が正しくない"
   end
-  
-  # タブストップ 8 なので、頭にタブがあった場合 minimum_indent は 8 (複数行)
-  public
+
+
+  # タブストップ 8 なので、minimum_indent は 8
   def test_minimum_indent_8_multiple_lines
-    assert_equal 8, (<<-TEST_STRING_MINIMUM_INDENT_8).minimum_indent, "minimum_indent の値がおかしい"
+    assert_equal 8, ( <<-TEST_STRING ).minimum_indent, "minimum_indent の返す値が正しくない"
 \t\tABCDE
 \tFGHIJ
 \t\tKLMNO
-    TEST_STRING_MINIMUM_INDENT_8
+    TEST_STRING
   end
 end
 
